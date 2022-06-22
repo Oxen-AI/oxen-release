ef021a785f954bba2c09291d1534e4b0f430138e
Greg Schoeninger <greg.schoeninger@gmail.com>
2022-06-22 08:29:50 -0700

    add merge test for fast forward modified

--- oxen-rust/src/lib/src/index/merger.rs
+++ oxen-rust/src/lib/src/index/merger.rs
@@ -15,7 +15,7 @@ impl Merger {
     }
 
     /// # Merge a branch name into the current checked out branch
-    pub fn merge<S: AsRef<str>>(&self, branch_name: S) -> Result<Option<Commit>, OxenError> {
+    pub fn merge<S: AsRef<str>>(&self, branch_name: S) -> Result<Commit, OxenError> {
         let branch_name = branch_name.as_ref();
         let ref_reader = RefReader::new(&self.repository)?;
         let head_commit_id = ref_reader.head_commit_id()?;
@@ -28,29 +28,34 @@ impl Merger {
         let merge_commit = commit_reader.get_commit_by_id(&merge_commit_id)?
                             .ok_or_else(|| OxenError::commit_db_corrupted(&merge_commit_id))?;
 
-        // TODO: This is just a fast forward merge, if we cannot traverse cleanly back from merge to HEAD
+        // TODO: 
         //       we will have to find the lowest common ancestor and try to merge from there
+        if self.needs_threeway_merge(&head_commit, &merge_commit) {
+            let commit = self.three_way_merge(head_commit, merge_commit)?;
+            Ok(commit)
+        } else {
+            let commit = self.fast_forward_merge(head_commit, merge_commit)?;
+            Ok(commit)
+        }
+    }
+
+    fn needs_threeway_merge(&self, head_commit: &Commit, merge_commit: &Commit) -> bool {
+        false
+    }
+
+    /// It is a fast forward merge if we cannot traverse cleanly back from merge to HEAD
+    fn fast_forward_merge(&self, head_commit: Commit, merge_commit: Commit) -> Result<Commit, OxenError> {
         let head_commit_entry_reader = CommitEntryReader::new(&self.repository, &head_commit)?;
         let merge_commit_entry_reader = CommitEntryReader::new(&self.repository, &merge_commit)?;
 
         let head_entries = head_commit_entry_reader.list_entries_set()?;
         let merge_entries = merge_commit_entry_reader.list_entries_set()?;
 
-        // Check if all the entries we want to merge exist
+        // Can just copy over all new versions since it is fast forward
         for merge_entry in merge_entries.iter() {
-            // Check if the one we want to merge is in HEAD
-            if let Some(head_entry) = head_entries.get(&merge_entry) {
-                // Check if the file contents is the same
-                if head_entry.hash != merge_entry.hash {
-                    // If it's different, we have to decide whether to take new, or try to merge automatically
-                    return Err(OxenError::basic_str("TODO, merge strategy"));
-                }
-            } else {
-                // We can just copy it over since it didn't exist before.
-                let version_file = util::fs::version_path(&self.repository, &merge_entry);
-                let dst_path = self.repository.path.join(&merge_entry.path);
-                std::fs::copy(version_file, dst_path)?;
-            }
+            let version_file = util::fs::version_path(&self.repository, &merge_entry);
+            let dst_path = self.repository.path.join(&merge_entry.path);
+            std::fs::copy(version_file, dst_path)?;
         }
 
         // Remove all entries that are in HEAD but not in merge entries
@@ -60,8 +65,12 @@ impl Merger {
                 std::fs::remove_file(path)?;
             }
         }
+        
+        Ok(merge_commit)
+    }
 
-        Ok(None)
+    fn three_way_merge(&self, head_commit: Commit, merge_commit: Commit) -> Result<Commit, OxenError> {
+        Ok(merge_commit)
     }
 }
 
@@ -153,4 +162,54 @@ mod tests {
             Ok(())
         })
     }
+
+    #[test]
+    fn test_one_commit_modified_fast_forward() -> Result<(), OxenError> {
+        test::run_empty_local_repo_test(|repo| {
+            // Write and add hello file
+            let og_branch = command::current_branch(&repo)?.unwrap();
+            let hello_file = repo.path.join("hello.txt");
+            util::fs::write_to_path(&hello_file, "Hello");
+            command::add(&repo, hello_file)?;
+
+            // Write and add world file
+            let world_file = repo.path.join("world.txt");
+            let og_contents = "World";
+            util::fs::write_to_path(&world_file, og_contents);
+            command::add(&repo, &world_file)?;
+
+            // Commit two files
+            command::commit(&repo, "Adding hello & world files")?;
+
+            // Branch to remove world
+            let branch_name = "modify-world";
+            command::create_checkout_branch(&repo, branch_name)?;
+
+            // Modify the file
+            let new_contents = "Around the world";
+            let world_file = test::modify_txt_file(world_file, new_contents)?;
+
+            // Commit the removal
+            command::add(&repo, &world_file)?;
+            command::commit(&repo, "Modifying world file")?;
+
+            // Checkout and merge additions
+            command::checkout(&repo, og_branch.name)?;
+            
+            // Make sure world file exists in it's original form
+            let contents = util::fs::read_from_path(&world_file)?;
+            assert_eq!(contents, og_contents);
+
+            let merger = Merger::new(&repo);
+            merger.merge(branch_name)?;
+
+            // Now that we've merged in, world file should be new content
+            let contents = util::fs::read_from_path(&world_file)?;
+            assert_eq!(contents, new_contents);
+
+            Ok(())
+        })
+    }
+
+    // TODO test for merge conflicts (if main branch gets ahead, need to write detection logic for this)
 }
