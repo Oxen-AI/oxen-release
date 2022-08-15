3fb995d6f648e7437117001afd40a0191bbcd5b3
Joshua Elliott <joshuacelliott@gmail.com>
2022-08-15 13:19:41 -0600

    Add dir controller

--- oxen-rust/src/server/src/controllers/entries.rs
+++ oxen-rust/src/server/src/controllers/entries.rs
@@ -8,7 +8,7 @@ use liboxen::index::CommitEntryReader;
 use liboxen::model::{Commit, CommitEntry, LocalRepository, RemoteEntry};
 use liboxen::util;
 use liboxen::view::http::{MSG_RESOURCE_CREATED, MSG_RESOURCE_FOUND, STATUS_SUCCESS};
-use liboxen::view::{PaginatedDirEntries, PaginatedEntries, RemoteEntryResponse, StatusMessage};
+use liboxen::view::{PaginatedEntries, RemoteEntryResponse, StatusMessage};
 
 use actix_web::{web, HttpRequest, HttpResponse};
 use flate2::read::GzDecoder;
@@ -22,16 +22,9 @@ use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 
 #[derive(Deserialize, Debug)]
-pub struct DirectoryPageNumQuery {
-    directory: Option<String>,
-    page_num: Option<usize>,
-    page_size: Option<usize>,
-}
-
-#[derive(Deserialize, Debug)]
 pub struct PageNumQuery {
-    page_num: Option<usize>,
-    page_size: Option<usize>,
+    pub page_num: Option<usize>,
+    pub page_size: Option<usize>,
 }
 
 pub async fn create(
@@ -308,187 +301,7 @@ pub async fn list_lines_in_file(req: HttpRequest, query: web::Query<PageNumQuery
     }
 }
 
-pub async fn list_files_for_head(
-    req: HttpRequest,
-    query: web::Query<DirectoryPageNumQuery>,
-) -> HttpResponse {
-    let app_data = req.app_data::<OxenAppData>().unwrap();
-
-    let namespace: &str = req.match_info().get("namespace").unwrap();
-    let name: &str = req.match_info().get("repo_name").unwrap();
-
-    // default to first page with first ten values
-    let page_num: usize = query.page_num.unwrap_or(1);
-    let page_size: usize = query.page_size.unwrap_or(10);
-    let directory = query
-        .directory
-        .clone()
-        .unwrap_or_else(|| String::from("./"));
-    let directory = Path::new(&directory);
-
-    log::debug!(
-        "list_files_for_head repo name [{}] directory: {:?} page_num {} page_size {}",
-        name,
-        directory,
-        page_num,
-        page_size,
-    );
-    match api::local::repositories::get_by_namespace_and_name(&app_data.path, namespace, name) {
-        Ok(Some(repo)) => {
-            log::debug!("list_files_for_head got repo [{}]", name);
-            if let Ok(commit) = api::local::commits::get_head_commit(&repo) {
-                match list_directory_for_commit(&repo, &commit.id, directory, page_num, page_size) {
-                    Ok((entries, _commit)) => HttpResponse::Ok().json(entries),
-                    Err(status_message) => HttpResponse::InternalServerError().json(status_message),
-                }
-            } else {
-                log::debug!(
-                    "list_files_for_head Could not find head commit for repo {}",
-                    name
-                );
-                HttpResponse::Ok().json(PaginatedDirEntries {
-                    status: String::from(STATUS_SUCCESS),
-                    status_message: String::from(MSG_RESOURCE_FOUND),
-                    page_size: 0,
-                    page_number: 0,
-                    total_pages: 0,
-                    total_entries: 0,
-                    entries: vec![],
-                })
-            }
-        }
-        Ok(None) => {
-            log::debug!("list_files_for_head Could not find repo with name {}", name);
-            HttpResponse::NotFound().json(StatusMessage::resource_not_found())
-        }
-        Err(err) => {
-            log::error!(
-                "list_files_for_head Unable to list directory {:?} in repo {}/{} for. Err: {}",
-                directory,
-                namespace,
-                name,
-                err
-            );
-            HttpResponse::InternalServerError().json(StatusMessage::internal_server_error())
-        }
-    }
-}
-
-pub async fn list_files_for_commit(
-    req: HttpRequest,
-    query: web::Query<DirectoryPageNumQuery>,
-) -> HttpResponse {
-    let app_data = req.app_data::<OxenAppData>().unwrap();
-
-    let namespace: &str = req.match_info().get("namespace").unwrap();
-    let name: &str = req.match_info().get("repo_name").unwrap();
-    let commit_id: &str = req.match_info().get("commit_id").unwrap();
-
-    // default to first page with first ten values
-    let page_num: usize = query.page_num.unwrap_or(1);
-    let page_size: usize = query.page_size.unwrap_or(10);
-    let directory = query
-        .directory
-        .clone()
-        .unwrap_or_else(|| String::from("./"));
-    let directory = Path::new(&directory);
-
-    log::debug!(
-        "list_files repo name [{}] commit_id [{}] directory: {:?} page_num {} page_size {}",
-        name,
-        commit_id,
-        directory,
-        page_num,
-        page_size,
-    );
-    match api::local::repositories::get_by_namespace_and_name(&app_data.path, namespace, name) {
-        Ok(Some(repo)) => {
-            log::debug!("list_files got repo [{}]", name);
-            match list_directory_for_commit(&repo, commit_id, directory, page_num, page_size) {
-                Ok((entries, _commit)) => HttpResponse::Ok().json(entries),
-                Err(status_message) => HttpResponse::InternalServerError().json(status_message),
-            }
-        }
-        Ok(None) => {
-            log::debug!("list_files Could not find repo with name {}", name);
-            HttpResponse::NotFound().json(StatusMessage::resource_not_found())
-        }
-        Err(err) => {
-            log::error!(
-                "list_files Unable to list directory {:?} in repo {}/{} for commit {}. Err: {}",
-                directory,
-                namespace,
-                name,
-                commit_id,
-                err
-            );
-            HttpResponse::InternalServerError().json(StatusMessage::internal_server_error())
-        }
-    }
-}
-
-fn list_directory_for_commit(
-    repo: &LocalRepository,
-    commit_id: &str,
-    directory: &Path,
-    page_num: usize,
-    page_size: usize,
-) -> Result<(PaginatedDirEntries, Commit), StatusMessage> {
-    match api::local::commits::get_by_id(repo, commit_id) {
-        Ok(Some(commit)) => {
-            log::debug!(
-                "list_directory_for_commit got commit [{}] '{}'",
-                commit.id,
-                commit.message
-            );
-            match api::local::entries::list_directory(
-                repo, &commit, directory, &page_num, &page_size,
-            ) {
-                Ok((entries, total_entries)) => {
-                    log::debug!(
-                        "list_directory_for_commit commit {} got {} entries",
-                        commit_id,
-                        entries.len()
-                    );
-
-                    let total_pages = total_entries as f64 / page_size as f64;
-                    let view = PaginatedDirEntries {
-                        status: String::from(STATUS_SUCCESS),
-                        status_message: String::from(MSG_RESOURCE_FOUND),
-                        page_size,
-                        page_number: page_num,
-                        total_pages: total_pages as usize,
-                        total_entries,
-                        entries,
-                    };
-                    Ok((view, commit))
-                }
-                Err(err) => {
-                    log::error!("Unable to list repositories. Err: {}", err);
-                    Err(StatusMessage::internal_server_error())
-                }
-            }
-        }
-        Ok(None) => {
-            log::debug!(
-                "list_directory_for_commit Could not find commit with id {}",
-                commit_id
-            );
-
-            Err(StatusMessage::resource_not_found())
-        }
-        Err(err) => {
-            log::error!(
-                "list_directory_for_commit Unable to get commit id {}. Err: {}",
-                commit_id,
-                err
-            );
-            Err(StatusMessage::internal_server_error())
-        }
-    }
-}
-
-fn get_entries_for_page(
+pub fn get_entries_for_page(
     repo: &LocalRepository,
     commit_id: &str,
     page_num: usize,
