543ffb3a3862c3611ec5b0a906f16a09c28c0476
Joshua Elliott <j@oxen.ai>
2022-10-08 15:11:17 -0600

    Add controller to resolve repo API URL

--- oxen-rust/src/server/src/controllers/version.rs
+++ oxen-rust/src/server/src/controllers/version.rs
@@ -1,5 +1,8 @@
+use crate::app_data::OxenAppData;
 use actix_web::{HttpRequest, HttpResponse};
+use liboxen::api;
 use liboxen::view::http::{MSG_RESOURCE_FOUND, STATUS_SUCCESS};
+use liboxen::view::StatusMessage;
 use serde::Serialize;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -18,4 +21,47 @@ pub async fn index(_req: HttpRequest) -> HttpResponse {
         version: String::from(VERSION),
     };
     HttpResponse::Ok().json(response)
+}
+
+#[derive(Serialize, Debug)]
+struct ResolveResponse {
+    pub status: String,
+    pub status_message: String,
+    pub repository_api_url: String,
+}
+
+pub async fn resolve(req: HttpRequest) -> HttpResponse {
+    let app_data = req.app_data::<OxenAppData>().unwrap();
+
+    let namespace: Option<&str> = req.match_info().get("namespace");
+    let name: Option<&str> = req.match_info().get("repo_name");
+    if let (Some(name), Some(namespace)) = (name, namespace) {
+        match api::local::repositories::get_by_namespace_and_name(&app_data.path, namespace, name) {
+            Ok(Some(_)) => match req.url_for("repo_root", &[namespace, name]) {
+                Ok(url) => {
+                    log::debug!("resolved repo URL: {}", url);
+                    HttpResponse::Ok().json(ResolveResponse {
+                        status: String::from(STATUS_SUCCESS),
+                        status_message: String::from(MSG_RESOURCE_FOUND),
+                        repository_api_url: url.to_string(),
+                    })
+                }
+                Err(err) => {
+                    log::debug!("Error generating repo URL: {:?}", err);
+                    HttpResponse::InternalServerError().json(StatusMessage::internal_server_error())
+                }
+            },
+            Ok(None) => {
+                log::debug!("404 Could not find repo: {}", name);
+                HttpResponse::NotFound().json(StatusMessage::resource_not_found())
+            }
+            Err(err) => {
+                log::debug!("Err finding repo: {} => {:?}", name, err);
+                HttpResponse::InternalServerError().json(StatusMessage::internal_server_error())
+            }
+        }
+    } else {
+        let msg = "Could not find `name` or `namespace` param...";
+        HttpResponse::BadRequest().json(StatusMessage::error(msg))
+    }
 }
