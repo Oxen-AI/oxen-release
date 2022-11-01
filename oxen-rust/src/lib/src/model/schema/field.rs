e72ebbbcb179bdb72a712f2a7e3e7768482c7585
Greg Schoeninger <greg.schoeninger@gmail.com>
2022-11-01 12:58:11 -0700

    update fields output in schemas command to collapse middle

--- oxen-rust/src/lib/src/model/schema/field.rs
+++ oxen-rust/src/lib/src/model/schema/field.rs
@@ -10,16 +10,19 @@ impl Field {
     pub fn fields_to_string<V: AsRef<Vec<Field>>>(fields: V) -> String {
         let fields = fields.as_ref();
         let max_num = 4;
-        let names: Vec<String> = fields
-            .iter()
-            .take(max_num)
-            .map(|f| f.name.to_owned())
-            .collect();
-
-        let combined_names = names.join(", ");
         if fields.len() > max_num {
-            format!("[{}, ...]", combined_names)
+            let name_0 = fields[0].name.to_owned();
+            let name_1 = fields[1].name.to_owned();
+            let name_2 = fields[fields.len() - 2].name.to_owned();
+            let name_3 = fields[fields.len() - 1].name.to_owned();
+
+            let combined_names = [name_0, name_1, String::from("..."), name_2, name_3].join(", ");
+            format!("[{}]", combined_names)
         } else {
+            let names: Vec<String> = fields.iter().map(|f| f.name.to_owned()).collect();
+
+            let combined_names = names.join(", ");
+
             format!("[{}]", combined_names)
         }
     }
