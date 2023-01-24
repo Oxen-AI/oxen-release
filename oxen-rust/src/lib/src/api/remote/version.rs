bbed5a580d5ff33e79dc723376d8be88022099d7
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-01-23 16:49:38 -0800

    change compatibility version to oxen_version

--- oxen-rust/src/lib/src/api/remote/version.rs
+++ oxen-rust/src/lib/src/api/remote/version.rs
@@ -12,9 +12,9 @@ pub async fn get_remote_version(host: &str) -> Result<String, OxenError> {
         log::debug!("get_remote_version got body: {}", body);
         let response: Result<VersionResponse, serde_json::Error> = serde_json::from_str(&body);
         match response {
-            Ok(val) => Ok(val.version),
+            Ok(val) => Ok(val.oxen_version),
             Err(_) => Err(OxenError::basic_str(format!(
-                "api::version::get_remote_version {} Err \n\n{}",
+                "api::version::get_remote_version {} Err parsing response \n\n{}",
                 url, body
             ))),
         }
--- oxen-rust/src/server/src/controllers/version.rs
+++ oxen-rust/src/server/src/controllers/version.rs
@@ -2,23 +2,16 @@ use crate::app_data::OxenAppData;
 use actix_web::{HttpRequest, HttpResponse};
 use liboxen::api;
 use liboxen::view::http::{MSG_RESOURCE_FOUND, STATUS_SUCCESS};
-use liboxen::view::StatusMessage;
+use liboxen::view::{StatusMessage, VersionResponse};
 use serde::Serialize;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
-
-#[derive(Serialize, Debug)]
-struct VersionResponse {
-    pub status: String,
-    pub status_message: String,
-    pub version: String,
-}
 
 pub async fn index(_req: HttpRequest) -> HttpResponse {
     let response = VersionResponse {
         status: String::from(STATUS_SUCCESS),
         status_message: String::from(MSG_RESOURCE_FOUND),
-        version: String::from(VERSION),
+        oxen_version: String::from(VERSION),
     };
     HttpResponse::Ok().json(response)
 }
