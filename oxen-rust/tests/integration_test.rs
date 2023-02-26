8b8ce5700d10ad0eec5fc3b69125dc76168a7118
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-02-26 11:24:46 -0800

    add test for rm then restore workflow

--- oxen-rust/tests/integration_test.rs
+++ oxen-rust/tests/integration_test.rs
@@ -2875,14 +2875,14 @@ fn test_status_rm_regular_file() -> Result<(), OxenError> {
         // Move the file to a new name
         let og_basename = PathBuf::from("README.md");
         let og_file = repo.path.join(&og_basename);
-        std::fs::remove_file(&og_file)?;
+        std::fs::remove_file(og_file)?;
 
         let status = command::status(&repo)?;
         status.print_stdout();
 
         assert_eq!(status.removed_files.len(), 1);
 
-        let opts = RmOpts::from_path(&og_file);
+        let opts = RmOpts::from_path(&og_basename);
         command::rm(&repo, &opts)?;
         let status = command::status(&repo)?;
         status.print_stdout();
@@ -2903,14 +2903,14 @@ fn test_status_rm_directory_file() -> Result<(), OxenError> {
         // Move the file to a new name
         let og_basename = PathBuf::from("README.md");
         let og_file = repo.path.join(&og_basename);
-        std::fs::remove_file(&og_file)?;
+        std::fs::remove_file(og_file)?;
 
         let status = command::status(&repo)?;
         status.print_stdout();
 
         assert_eq!(status.removed_files.len(), 1);
 
-        let opts = RmOpts::from_path(&og_file);
+        let opts = RmOpts::from_path(&og_basename);
         command::rm(&repo, &opts)?;
         let status = command::status(&repo)?;
         status.print_stdout();
@@ -2920,6 +2920,60 @@ fn test_status_rm_directory_file() -> Result<(), OxenError> {
             status.added_files[&og_basename].status,
             StagedEntryStatus::Removed
         );
+
+        Ok(())
+    })
+}
+
+/// Should be able to use `oxen rm -r` then restore to get files back
+///
+/// $ oxen rm -r train/
+/// $ oxen restore --staged train/
+/// $ oxen restore train/
+#[test]
+fn test_rm_directory_restore_directory() -> Result<(), OxenError> {
+    test::run_training_data_repo_test_fully_committed(|repo| {
+        let rm_dir = PathBuf::from("train");
+        let full_path = repo.path.join(&rm_dir);
+        let num_files = util::fs::rcount_files_in_dir(&full_path);
+
+        // Remove directory
+        let opts = RmOpts {
+            path: rm_dir.to_owned(),
+            recursive: true,
+            staged: false,
+        };
+        command::rm(&repo, &opts)?;
+
+        // Make sure we staged these removals
+        let status = command::status(&repo)?;
+        status.print_stdout();
+        assert_eq!(num_files, status.added_files.len());
+        for (_path, entry) in status.added_files.iter() {
+            assert_eq!(entry.status, StagedEntryStatus::Removed);
+        }
+        // Make sure directory is no longer on disk
+        assert!(!full_path.exists());
+
+        // Restore the content from staging area
+        let opts = RestoreOpts::from_staged_path(&rm_dir);
+        command::restore(&repo, opts)?;
+
+        // This should have removed all the staged files, but not restored from disk yet.
+        let status = command::status(&repo)?;
+        status.print_stdout();
+        assert_eq!(0, status.added_files.len());
+        assert_eq!(num_files, status.removed_files.len());
+
+        // This should restore all the files from the HEAD commit
+        let opts = RestoreOpts::from_path(&rm_dir);
+        command::restore(&repo, opts)?;
+
+        let status = command::status(&repo)?;
+        status.print_stdout();
+
+        let num_restored = util::fs::rcount_files_in_dir(&full_path);
+        assert_eq!(num_restored, num_files);
 
         Ok(())
     })
