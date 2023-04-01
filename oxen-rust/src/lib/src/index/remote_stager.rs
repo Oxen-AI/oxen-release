acdd4ec38ad6e1d0b8a140bfe98dbaf8a548e83d
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-04-01 11:19:29 -0700

    fix remote status to add modified files to added area

--- oxen-rust/src/lib/src/index/remote_stager.rs
+++ oxen-rust/src/lib/src/index/remote_stager.rs
@@ -43,20 +43,21 @@ pub async fn status(
 
     let mut status = StagedData::empty();
     status.added_dirs = remote_status.added_dirs;
-    status.added_files =
+    let added_files: HashMap<PathBuf, StagedEntry> =
         HashMap::from_iter(remote_status.added_files.entries.into_iter().map(|e| {
             (
                 PathBuf::from(e.filename),
                 StagedEntry::empty_status(StagedEntryStatus::Added),
             )
         }));
-    status.modified_files = Vec::from_iter(
-        remote_status
-            .modified_files
-            .entries
-            .into_iter()
-            .map(|e| PathBuf::from(e.filename)),
-    );
+    let added_mods: HashMap<PathBuf, StagedEntry> =
+        HashMap::from_iter(remote_status.modified_files.entries.into_iter().map(|e| {
+            (
+                PathBuf::from(e.filename),
+                StagedEntry::empty_status(StagedEntryStatus::Modified),
+            )
+        }));
+    status.added_files = added_files.into_iter().chain(added_mods).collect();
 
     Ok(status)
 }
