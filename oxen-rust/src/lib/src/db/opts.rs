f63925cb22064d508550a156043942b9da28069c
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-04-06 15:06:21 -0700

    clippy & format

--- oxen-rust/src/lib/src/db/opts.rs
+++ oxen-rust/src/lib/src/db/opts.rs
@@ -1,13 +1,13 @@
 use rocksdb::{LogLevel, Options};
 
-
 pub fn default() -> Options {
     let mut opts = Options::default();
     opts.set_log_level(LogLevel::Fatal);
     opts.create_if_missing(true);
     opts.set_max_log_file_size(0);
     opts.set_keep_log_file_num(1);
-    let max_open_files = std::env::var("MAX_OPEN_FILES").map_or(128, |v| v.parse().expect("MAX_OPEN_FILES must be a number"));
+    let max_open_files = std::env::var("MAX_OPEN_FILES")
+        .map_or(128, |v| v.parse().expect("MAX_OPEN_FILES must be a number"));
     opts.set_max_open_files(max_open_files);
     opts
 }
