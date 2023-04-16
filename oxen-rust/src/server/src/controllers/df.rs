80999a784468ad03f57a9a2b3234e29b4159a790
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-04-16 15:34:46 -0700

    add --head and --tail params to df command

--- oxen-rust/src/server/src/controllers/df.rs
+++ oxen-rust/src/server/src/controllers/df.rs
@@ -36,32 +36,28 @@ pub async fn get(req: HttpRequest, query: web::Query<DFOptsQuery>) -> HttpRespon
                 opts = df_opts_query::parse_opts(&query, &mut opts);
 
                 match util::fs::version_path_for_commit_id(&repo, &commit_id, &filepath) {
-                    Ok(version_path) => match tabular::scan_df(&version_path, &opts) {
-                        Ok(lazy_df) => {
+                    Ok(version_path) => match tabular::read_df(&version_path, opts) {
+                        Ok(mut df) => {
                             log::debug!("Read version file {:?}", version_path);
 
-                            log::debug!("Got opts {:?}", opts);
-                            let lazy_cp = lazy_df.clone();
-                            let mut df = tabular::transform_lazy(lazy_cp, opts).unwrap();
-                            let full_df = lazy_df.collect().unwrap();
                             let page_size = query.page_size.unwrap_or(constants::DEFAULT_PAGE_SIZE);
                             let page = query.page.unwrap_or(constants::DEFAULT_PAGE_NUM);
 
                             let total_pages =
-                                (full_df.height() as f64 / page_size as f64).ceil() as usize;
+                                (df.height() as f64 / page_size as f64).ceil() as usize;
 
                             let response = JsonDataFrameSliceResponse {
                                 status: String::from(STATUS_SUCCESS),
                                 status_message: String::from(MSG_RESOURCE_FOUND),
-                                df: JsonDataFrame::from_df(&mut df),
                                 full_size: JsonDataSize {
-                                    width: full_df.width(),
-                                    height: full_df.height(),
+                                    width: df.width(),
+                                    height: df.height(),
                                 },
+                                df: JsonDataFrame::from_df(&mut df),
                                 page_number: page,
                                 page_size,
                                 total_pages,
-                                total_entries: full_df.height(),
+                                total_entries: df.height(),
                             };
                             HttpResponse::Ok().json(response)
                         }
