93cf9a15cc99649a63dc322493db60410b344d2f
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-04-27 17:22:16 -0700

    add test and impl for oxen clone, and fix oxen add parallelism

--- oxen/tests/test_commit.py
+++ oxen/tests/test_commit.py
@@ -1,19 +1,12 @@
 import os
 
-from oxen import Repo
 
-
-def test_commit(shared_datadir):
-    # Use the shared_datadir fixture to get the path to the CelebA dataset
-    repo_dir = os.path.join(shared_datadir, "CelebA")
-
-    # oxen init
-    repo = Repo(repo_dir)
-    repo.init()
+def test_commit_one_file(celeba_local_repo_no_commits):
+    repo = celeba_local_repo_no_commits
 
     # oxen add
     image_file = "images/1.jpg"
-    full_path = os.path.join(repo_dir, image_file)
+    full_path = os.path.join(repo.path, image_file)
     repo.add(full_path)
 
     # oxen commit
@@ -23,3 +16,23 @@ def test_commit(shared_datadir):
     history = repo.log()
     # There is always an initial commit + the one we just made
     assert len(history) == 2
+
+
+def test_commit_all(celeba_local_repo_no_commits):
+    repo = celeba_local_repo_no_commits
+
+    # oxen add images
+    repo.add(os.path.join(repo.path, "annotations"))
+
+    # oxen commit
+    repo.commit("Add all images")
+
+    # oxen add annotations
+    repo.add(os.path.join(repo.path, "annotations"))
+
+    # oxen commit
+    repo.commit("Add all annotations")
+
+    # oxen log
+    history = repo.log()
+    assert len(history) == 3
