a6c247d498ee6f726824cb28a1651f6e674cdf7a
Ben Artuso <ben@bens-mbp-3.mynetworksettings.com>
2023-05-10 15:32:04 -0400

    add docstrings, refine tests, update branch v. revision

--- oxen-python/src/py_remote_repo.rs
+++ oxen-python/src/py_remote_repo.rs
@@ -2,22 +2,20 @@ use pyo3::prelude::*;
 
 use std::collections::HashMap;
 
-
-use liboxen::model::{CommitBody, Remote, RemoteRepository, StagedData, StagedEntry, StagedEntryStatus};
-use liboxen::{api, command};
-use liboxen::api;
 use liboxen::config::UserConfig;
+use liboxen::model::{
+    CommitBody, Remote, RemoteRepository, StagedData, StagedEntry, StagedEntryStatus,
+};
+use liboxen::{api, command};
 
 use pyo3::exceptions::PyValueError;
 use std::path::PathBuf;
 
 use crate::branch::PyBranch;
-use crate::py_commit::PyCommit;
 use crate::error::PyOxenError;
+use crate::py_commit::PyCommit;
 
-use std::path::PathBuf;
 use crate::py_staged_data::PyStagedData;
-
 
 #[pyclass]
 pub struct PyRemoteRepo {
@@ -117,17 +115,31 @@ impl PyRemoteRepo {
         Ok(())
     }
 
-    fn add(&self, branch_name: String, directory_name: String, path: PathBuf,) -> Result<(), PyOxenError> {
+    fn add(
+        &self,
+        branch_name: String,
+        directory_name: String,
+        path: PathBuf,
+    ) -> Result<(), PyOxenError> {
         let user_id = UserConfig::identifier()?;
-        pyo3_asyncio::tokio::get_runtime()
-            .block_on(async { api::remote::staging::add_file(&self.repo, &branch_name, &user_id, &directory_name, path).await})?;
+        pyo3_asyncio::tokio::get_runtime().block_on(async {
+            api::remote::staging::add_file(
+                &self.repo,
+                &branch_name,
+                &user_id,
+                &directory_name,
+                path,
+            )
+            .await
+        })?;
         Ok(())
     }
 
     fn remove(&self, branch_name: String, path: PathBuf) -> Result<(), PyOxenError> {
         let user_id = UserConfig::identifier()?;
-        pyo3_asyncio::tokio::get_runtime()
-            .block_on(async { api::remote::staging::rm_staged_file(&self.repo, &branch_name, &user_id, path).await})?;
+        pyo3_asyncio::tokio::get_runtime().block_on(async {
+            api::remote::staging::rm_staged_file(&self.repo, &branch_name, &user_id, path).await
+        })?;
         Ok(())
     }
 
@@ -135,37 +147,54 @@ impl PyRemoteRepo {
         let user_id = UserConfig::identifier()?;
         let user = UserConfig::get()?.to_user();
         let commit = CommitBody { message, user };
-        pyo3_asyncio::tokio::get_runtime()
-            .block_on(async { api::remote::staging::commit_staged(&self.repo, &branch_name, &user_id, &commit).await })?;
+        pyo3_asyncio::tokio::get_runtime().block_on(async {
+            api::remote::staging::commit_staged(&self.repo, &branch_name, &user_id, &commit).await
+        })?;
         Ok(())
     }
 
     fn log(&self, branch_name_or_commit_id: String) -> Result<Vec<PyCommit>, PyOxenError> {
-        let log = pyo3_asyncio::tokio::get_runtime()
-            .block_on(async { api::remote::commits::list_commit_history(&self.repo, &branch_name_or_commit_id).await })?;
+        let log = pyo3_asyncio::tokio::get_runtime().block_on(async {
+            api::remote::commits::list_commit_history(&self.repo, &branch_name_or_commit_id).await
+        })?;
         Ok(log.iter().map(|c| PyCommit { commit: c.clone() }).collect())
     }
 
     fn list_branches(&self) -> Result<Vec<PyBranch>, PyOxenError> {
         let branches = pyo3_asyncio::tokio::get_runtime()
             .block_on(async { api::remote::branches::list(&self.repo).await })?;
-        Ok(branches.iter().map(|b| PyBranch { name: b.name.clone(), commit_id: b.commit_id.clone() }).collect())
+        Ok(branches
+            .iter()
+            .map(|b| PyBranch {
+                name: b.name.clone(),
+                commit_id: b.commit_id.clone(),
+            })
+            .collect())
     }
-    
+
     fn status(&self, branch_name: String, path: PathBuf) -> Result<PyStagedData, PyOxenError> {
         let user_id = UserConfig::identifier()?;
-        let remote_status = pyo3_asyncio::tokio::get_runtime()
-            .block_on(async { api::remote::staging::status(&self.repo, &branch_name, &user_id, &path, liboxen::constants::DEFAULT_PAGE_NUM, liboxen::constants::DEFAULT_PAGE_SIZE).await})?;
-        
+        let remote_status = pyo3_asyncio::tokio::get_runtime().block_on(async {
+            api::remote::staging::status(
+                &self.repo,
+                &branch_name,
+                &user_id,
+                &path,
+                liboxen::constants::DEFAULT_PAGE_NUM,
+                liboxen::constants::DEFAULT_PAGE_SIZE,
+            )
+            .await
+        })?;
+
         let mut status = StagedData::empty();
         status.added_dirs = remote_status.added_dirs;
         let added_files: HashMap<PathBuf, StagedEntry> =
-        HashMap::from_iter(remote_status.added_files.entries.into_iter().map(|e| {
-            (
-                PathBuf::from(e.filename),
-                StagedEntry::empty_status(StagedEntryStatus::Added),
-            )
-        }));
+            HashMap::from_iter(remote_status.added_files.entries.into_iter().map(|e| {
+                (
+                    PathBuf::from(e.filename),
+                    StagedEntry::empty_status(StagedEntryStatus::Added),
+                )
+            }));
         let added_mods: HashMap<PathBuf, StagedEntry> =
             HashMap::from_iter(remote_status.modified_files.entries.into_iter().map(|e| {
                 (
@@ -176,7 +205,7 @@ impl PyRemoteRepo {
         status.added_files = added_files.into_iter().chain(added_mods).collect();
 
         Ok(PyStagedData { data: status })
-        }
+    }
 
     fn get_branch(&self, branch_name: String) -> PyResult<PyBranch> {
         log::info!("Get branch... {branch_name}");
@@ -195,7 +224,7 @@ impl PyRemoteRepo {
         }
     }
 
-    fn create_or_get_branch(&self, new_name: String, from_name: String) -> PyResult<PyBranch> {
+    fn create_branch(&self, new_name: String, from_name: String) -> PyResult<PyBranch> {
         log::info!("create from or get branch... {new_name} from {from_name}");
         log::info!("From repo... {}", self.repo.remote.url);
         let branch = pyo3_asyncio::tokio::get_runtime().block_on(async {
@@ -205,9 +234,9 @@ impl PyRemoteRepo {
         match branch {
             Ok(branch) => Ok(PyBranch {
                 name: branch.name,
-                commit_id: branch.commit_id
+                commit_id: branch.commit_id,
             }),
-            _ => Err(PyValueError::new_err("Could not get or create branch"))
+            _ => Err(PyValueError::new_err("Could not get or create branch")),
         }
     }
 }
--- oxen-python/tests/test_remote_branch.py
+++ oxen-python/tests/test_remote_branch.py
@@ -1,12 +1,18 @@
-import os
 from oxen import RemoteRepo
 
-def test_create_get_new_branch(empty_remote_repo: RemoteRepo, shared_datadir):
-    empty_remote_repo.get_branch('main')
 
-def test_create_get_existing_branch(celeba_remote_repo_one_image_pushed: RemoteRepo, shared_datadir):
+def test_create_existing_branch(
+    celeba_remote_repo_one_image_pushed: RemoteRepo, shared_datadir
+):
     _, remote_repo = celeba_remote_repo_one_image_pushed
-    remote_repo.create_or_get_branch('hrllo')
-    remote_repo.get_branch('hrllo')
+    remote_repo.create_branch("hrllo")
+    remote_repo.get_branch("hrllo")
+    # TODO: Getters for branch name?
 
 
+def test_get_existing_branch(
+    celeba_remote_repo_one_image_pushed: RemoteRepo, shared_datadir
+):
+    _, remote_repo = celeba_remote_repo_one_image_pushed
+    remote_repo.get_branch("main")
+    # TODO: Getters for branch name?
