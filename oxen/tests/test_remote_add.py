d3a4ab1d4f7e2ee264691b162d760c79fe858051
Ben Artuso <ben@bens-mbp-3.mynetworksettings.com>
2023-05-10 12:13:55 -0400

    add more remote functionality

--- oxen/tests/test_remote_add.py
+++ oxen/tests/test_remote_add.py
@@ -1,18 +1,34 @@
 import os
 from oxen import RemoteRepo
 
-# def test_remote_add_file_new_branch(empty_remote_repo: RemoteRepo, shared_datadir):
-#      full_path = os.path.join(shared_datadir, "CelebA/images/1.jpg")
-#      empty_remote_repo.create_or_get_branch('main')
-#      empty_remote_repo.add(full_path, 'main', '')
-    
+def test_remote_add_single_file(celeba_remote_repo_one_image_pushed: RemoteRepo, shared_datadir):
+    full_path = os.path.join(shared_datadir, "CelebA/images/1.jpg")
 
-def test_remote_add_file_existing_branch(celeba_remote_repo_one_image_pushed: RemoteRepo, shared_datadir):
     _, remote_repo = celeba_remote_repo_one_image_pushed
-    print(remote_repo.create_or_get_branch("maindo"))
-    print(remote_repo.create_or_get_branch("maindondo"))
-    print(remote_repo.create_or_get_branch("maindolt"))
-    print(remote_repo.create_or_get_branch("ajfdkldsf"))
+
+
+    remote_repo.add(full_path, "main", "a-folder")
+    staged_data = remote_repo.status('main', "a-folder")
+    added_files = staged_data.added_files()
+
+    assert added_files == ["a-folder/1.jpg"]
+
+def test_remote_add_root_dir(celeba_remote_repo_one_image_pushed: RemoteRepo, shared_datadir):
+    full_path = os.path.join(shared_datadir, "CelebA/images/3.jpg")
+
+    _, remote_repo = celeba_remote_repo_one_image_pushed
+
+
+    remote_repo.add(full_path, "main", "")
+    staged_data = remote_repo.status('main', "")
+    added_files = staged_data.added_files()
+    print(added_files)
+
+    assert added_files == ["3.jpg"]
+
+
+
+    
     
 
 
