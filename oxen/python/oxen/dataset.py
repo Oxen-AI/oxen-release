59bd4dd487aa047050a6646483850933dee3a788
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-05-10 18:05:38 -0700

    initial dataset tests starting to pass

--- oxen/python/oxen/dataset.py
+++ oxen/python/oxen/dataset.py
@@ -50,7 +50,14 @@ class Dataset:
     """
 
     # TODO: allow remote or local repos
-    def __init__(self, repo, cache_dir: str = None):
+    def __init__(
+        self,
+        repo,
+        paths: Union[str, Sequence[str]],
+        features=None,
+        cache_dir: str = None,
+        download: bool = False,
+    ):
         """
         Create a new RemoteRepo object to interact with.
 
@@ -59,8 +66,14 @@ class Dataset:
         repo : Repo
             The oxen repository you are loading data from
             can be a local or a remote repo
+        features : Features | None
+            The features of the dataset, columns, dtypes, etc.
+        paths : str | Sequence[str]
+            The paths to the data files needed to load the dataset
         cache_dir : str
             The directory to download/cache the data in.
+        download : bool
+            Whether to download the data or not.
         """
         self._repo = repo
         if cache_dir is None:
@@ -68,39 +81,67 @@ class Dataset:
         else:
             self._cache_dir = cache_dir
 
-        self._data_files = []
+        if isinstance(paths, str):
+            self._paths = [paths]
+        else:
+            self._paths = paths
+
         self._data_frames = []
+        self._features = features
+
+        if download:
+            self.download_all()
+
+    # For iterating over the dataset
+    def __len__(self):
+        return sum([df.height for df in self._data_frames])
+
+    # For iterating over the dataset
+    def __getitem__(self, idx):
+        # iterate over data frames to find the right one to index
+        df_offset = 0  # which row we are at
+        df_idx = 0  # which dataframe we are on
+        while df_offset < idx:
+            df_offset += self._data_frames[df_idx].height
+            df_idx += 1
+
+        # Offset is the row we are at in the data frame
+        remainder = idx - df_offset
+
+        # extract the features from the data frame
+        ret_features = []
+        for feature in self.features:
+            df = self._data_frames[df_idx]
+            val = df[feature.name][remainder]
+            ret_features.append(val)
+
+        return ret_features
 
     @staticmethod
     def default_cache_dir(repo) -> str:
-        # ~/.oxen/data/<namespace>/<repo>/<revision>
+        # ~/.oxen/.cache/<namespace>/<repo>/<revision>
         cache_dir = os.path.join(Path.home(), ".oxen")
-        cache_dir = os.path.join(cache_dir, "data")
+        cache_dir = os.path.join(cache_dir, ".cache")
         cache_dir = os.path.join(cache_dir, repo.namespace)
         cache_dir = os.path.join(cache_dir, repo.name)
         cache_dir = os.path.join(cache_dir, repo.revision)
         return cache_dir
 
-    def _cache_path(self, path: str) -> str:
-        """
-        Returns the path to the file in the cache directory.
-        """
-        return os.path.join(self._cache_dir, path)
-
-    def _df_download_path(self, path, base_dir: str | None) -> str:
+    def _cache_download_path(self, base_dir: str | None) -> str:
         """
         Returns the path to the file given the base dir or defaults to
         the cache directory.
         """
         if base_dir is None:
-            return self._cache_path(path)
-        return os.path.join(base_dir, path)
+            return self._cache_dir
+        return base_dir
 
     # TODO: optionally download data
     def df(self, path: str, base_dir: str = None) -> pl.DataFrame:
         """
-        Returns a dataframe of the data from the repo.
-            Parameters
+        Returns a dataframe of the data from the repo, fully loaded into memory.
+
+        Parameters
         ----------
         path : str
             Paths to the data frames you want to load.
@@ -110,38 +151,38 @@ class Dataset:
         # TODO:
         #  * handle streaming into memory without downloading to disk
 
-        # Download file to cache path if it does not already exist
-        # Cache path has the revision, so if the revision changes, it will
-        # download the new data
-        output_path = self._df_download_path(path, base_dir)
-        if not os.path.exists(output_path):
-            os.makedirs(os.path.dirname(output_path))
-            self._repo.download(path, output_path)
-
+        output_path = self.download(path, base_dir)
         df = PyDataset.df(output_path)
         return df
 
-    def load(
-        self, data: Union[str, Sequence[str]], download: bool = True
-    ) -> pl.DataFrame:
+    def download(self, remote_path: str, base_dir: str = None) -> str:
         """
-        Load the data from the repo into a dataframe.
+        Returns the path to the downloaded file.
 
         Parameters
         ----------
-        data : str | Sequence[str]
-            Path or paths to the data frames you want to load.
-
-            If you pass a string, it will pull the data from the  at that path.
-            If you pass a sequence, it will
+        path : str
+            Paths to the file you want to load.
+        base_dir : str | None
+            The base directory to download the data to.
         """
-        if isinstance(data, str):
-            self._data_files = [data]
-        else:
-            self._data_files = data
+        # Download file to cache path if it does not already exist
+        # Cache path has the revision, so if the revision changes, it will
+        # download the new data
+        base_dir = self._cache_download_path(base_dir)
+        output_path = os.path.join(base_dir, remote_path)
+        parent = os.path.dirname(output_path)
+        if not os.path.exists(parent):
+            os.makedirs(parent, exist_ok=True)
+        if not os.path.exists(output_path):
+            self._repo.download(remote_path, parent)
+        return output_path
 
-        if download:
-            for data_file in self._data_files:
-                print(f"Downloading {data_file}...")
-                df = self.df(data_file, self._cache_dir)
-                self._data_frames.append(df)
+    def download_all(self):
+        """
+        Download the data from the repo to disk
+        """
+        for path in self._paths:
+            print(f"Downloading {path}...")
+
+            path = self.download(path, self._cache_dir)
