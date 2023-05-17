bc4ac32ffaa035b55aa42b92e61d9c1fc6930a5d
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-05-16 17:12:06 -0700

    add sphinx for docs and update readme to have appropriate links

---
+++ oxen-python/docs/source/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import toml
+
+with open("../../Cargo.toml") as f:
+    data = toml.load(f)
+
+version = data["package"]["version"]
+
+project = '🐂 🌾 Oxen'
+copyright = '2023, OxenAI'
+author = 'Greg Schoeninger'
+release = version
+
+html_title = project + " " + version
+html_last_updated_fmt = "%b %d, %Y"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "m2r2",
+    "sphinx_copybutton",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+source_suffix = [".md", ".rst"]
+
+m2r_parse_relative_links = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ['_static']
---
+++ oxen-python/docs/source/index.rst
@@ -0,0 +1,17 @@
+.. OxenAI documentation master file, created by
+   sphinx-quickstart on Tue May 16 13:02:04 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+.. mdinclude:: ../../../README.md
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Getting Started
+   :name: getting_started
+   :hidden:
+
+   getting_started/command_line.md
+   getting_started/python.md
+   getting_started/tutorials.md
