773a2018e4456625327e5b04643401b923c1420d
Ben Artuso <ben@Bens-MacBook-Pro-3.local>
2023-06-05 16:01:58 -0400

    adding arg expectation docstrings

--- oxen/python/oxen/ops/encode_labels.py
+++ oxen/python/oxen/ops/encode_labels.py
@@ -3,6 +3,15 @@ import numpy as np
 
 
 class EncodeLabels(oxen.Op):
+    '''
+    Maps a column of labels to a column of integers according to a dictionary.
+
+    Args:
+        args[0] : polars.Series
+            Polars series of labels to be mapped
+        args[1] : Dict[str, int]
+            Dictionary mapping labels to integers
+    '''
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
