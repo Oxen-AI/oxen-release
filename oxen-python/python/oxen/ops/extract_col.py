f6e742a579b077191b9c2a19875d1b2d50023e60
Ben Artuso <ben@Bens-MacBook-Pro-3.local>
2023-06-05 16:01:58 -0400

    adding arg expectation docstrings

--- oxen-python/python/oxen/ops/extract_col.py
+++ oxen-python/python/oxen/ops/extract_col.py
@@ -3,6 +3,15 @@ import oxen
 
 class ExtractCol(oxen.Op):
     def __init__(self, *args, **kwargs):
+        '''
+        Extracts a column from a DataFrame.
+
+        Args:
+            args[0]: polars.DataFrame
+                DataFrame to extract column from
+            args[1]: str
+                Name of column to extract
+        '''
         super().__init__(*args, **kwargs)
 
     def call(self, args):
--- oxen-python/python/oxen/ops/read_image_dir.py
+++ oxen-python/python/oxen/ops/read_image_dir.py
@@ -5,6 +5,15 @@ import cv2
 
 
 class ReadImageDir(oxen.Op):
+    '''
+    Reads in imagery as specified by a DataFrame column of paths. 
+
+    Args:
+        args[0] : str
+            Root imagery directory, is prefixed to DataFrame paths
+        args[1] : List[str]
+            Column of paths to imagery (relative to root directory specified in args[0])
+    '''
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -14,7 +23,5 @@ class ReadImageDir(oxen.Op):
         for path in tqdm(args[1]):
             img = cv2.imread(f"{prefix}/{path}")
             image_data.append(img)
-        # TODO: Todo: handle differing image shapes, or leave that to user?
-        # For now, assuming all images are same shape
         return np.array(image_data)
 
