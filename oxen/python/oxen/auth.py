8999f0e8aa2453d7b28cab515811d917cb7f1fdc
Ben Artuso <ben@Bens-MacBook-Pro-3.local>
2023-06-06 13:50:35 -0400

    validate path

--- oxen/python/oxen/auth.py
+++ oxen/python/oxen/auth.py
@@ -5,7 +5,7 @@ def create_user_config(name: str, email: str, path: str | None = None):
     if path is None:
         path = f"{auth.get_oxen_home_dir()}/user_config.toml"
     if not path.endswith(".toml"):
-        path = f"{path}/user_config.toml"
+        raise ValueError("Path must end with .toml")
     auth.create_user_config(name, email, path)
 
 
@@ -13,5 +13,5 @@ def add_host_auth(name: str, email: str, path: str | None = None):
     if path is None:
         path = f"{auth.get_oxen_home_dir()}/user_config.toml"
     if not path.endswith(".toml"):
-        path = f"{path}/user_config.toml"
+        raise ValueError("Path must end with .toml")
     auth.add_host_auth(name, email, path)
