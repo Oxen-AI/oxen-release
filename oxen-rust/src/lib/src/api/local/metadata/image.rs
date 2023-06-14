a9351b598e33d7cdb93730e52ab6b8077fe0e172
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-06-14 15:18:39 -0700

    WIP: querying duckdb metadata database

--- oxen-rust/src/lib/src/api/local/metadata/image.rs
+++ oxen-rust/src/lib/src/api/local/metadata/image.rs
@@ -2,145 +2,125 @@
 //!
 
 use crate::error::OxenError;
-use crate::model::entry::metadata_entry::MetaDataImage;
+use crate::model::entry::metadata_entry::{ImgColorSpace, MetaDataImage};
 
-// use magick_rust::{magick_wand_genesis, MagickWand};
-
+use image::GenericImageView;
 use std::path::Path;
-// use std::sync::Once;
-
-// static START: Once = Once::new();
 
 /// Detects the image metadata for the given file.
-pub fn get_metadata(_path: impl AsRef<Path>) -> Result<MetaDataImage, OxenError> {
-    // START.call_once(|| {
-    //     magick_wand_genesis();
-    // });
-
-    // let path = path.as_ref();
-
-    // let wand = MagickWand::new();
-    // match wand.ping_image(&path.to_string_lossy()) {
-    //     Ok(_) => {}
-    //     Err(err) => {
-    //         let err = format!("Could not get image metadata {:?} Err {:?}", path, err);
-    //         log::warn!("{}", err);
-    //         return Err(OxenError::image_metadata_error(&err));
-    //     }
-    // }
-
-    // let format = match wand.get_image_format() {
-    //     Ok(format) => format,
-    //     Err(err) => {
-    //         log::warn!("Could not get image format for {:?} Err {:?}", path, err);
-    //         "Unknown".to_string()
-    //     }
-    // };
-
-    // Ok(MetaDataImage {
-    //     width: wand.get_image_width(),
-    //     height: wand.get_image_height(),
-    //     color_space: magick_to_colorspace(&wand),
-    //     format,
-    // })
-    Err(OxenError::basic_str("Could not get image metadata"))
+pub fn get_metadata(path: impl AsRef<Path>) -> Result<MetaDataImage, OxenError> {
+    match image::open(path) {
+        Ok(img) => {
+            let (width, height) = img.dimensions();
+            let color_space = image_to_colorspace(&img.color());
+            Ok(MetaDataImage {
+                width: width as usize,
+                height: height as usize,
+                color_space,
+            })
+        }
+        Err(e) => {
+            log::error!("Could not get image metadata {:?}", e);
+            Err(OxenError::basic_str("Could not get image metadata"))
+        }
+    }
 }
 
-// fn magick_to_colorspace(wand: &MagickWand) -> ImgColorSpace {
-//     let colorspace = wand.get_image_colorspace();
-//     match colorspace {
-//         magick_rust::bindings::ColorspaceType_RGBColorspace
-//         | magick_rust::bindings::ColorspaceType_sRGBColorspace => {
-//             if wand.get_image_alpha_channel() {
-//                 ImgColorSpace::RGBA
-//             } else {
-//                 ImgColorSpace::RGB
-//             }
-//         }
-//         magick_rust::bindings::ColorspaceType_GRAYColorspace => ImgColorSpace::Grayscale,
-//         _ => ImgColorSpace::Unknown,
-//     }
-// }
+fn image_to_colorspace(colorspace: &image::ColorType) -> ImgColorSpace {
+    match colorspace {
+        image::ColorType::L8 => ImgColorSpace::Grayscale,
+        image::ColorType::La8 => ImgColorSpace::GrayscaleAlpha,
+        image::ColorType::Rgb8 => ImgColorSpace::RGB,
+        image::ColorType::Rgba8 => ImgColorSpace::RGBA,
+        image::ColorType::L16 => ImgColorSpace::Grayscale16,
+        image::ColorType::La16 => ImgColorSpace::GrayscaleAlpha16,
+        image::ColorType::Rgb16 => ImgColorSpace::Rgb16,
+        image::ColorType::Rgba16 => ImgColorSpace::Rgba16,
+        image::ColorType::Rgb32F => ImgColorSpace::Rgb32F,
+        image::ColorType::Rgba32F => ImgColorSpace::Rgba32F,
+        _ => ImgColorSpace::Unknown,
+    }
+}
 
 #[cfg(test)]
 mod tests {
 
+    use crate::api;
+    use crate::model::entry::entry_data_type::EntryDataType;
+    use crate::model::entry::metadata_entry::ImgColorSpace;
+    use crate::test;
+
     #[test]
     fn test_get_metadata_img_rgb() {
-        // let file = test::test_img_file_with_name("cat_1.jpg");
+        let file = test::test_img_file_with_name("cat_1.jpg");
 
-        // let data = api::local::metadata::compute_metadata(file).unwrap();
+        let data = api::local::metadata::compute_metadata(file).unwrap();
 
-        // assert_eq!(data.data_type, EntryDataType::Image);
-        // assert_eq!(data.mime_type, "image/jpeg");
+        assert_eq!(data.data_type, EntryDataType::Image);
+        assert_eq!(data.mime_type, "image/jpeg");
 
-        // let metadata = data.meta.image.unwrap();
-        // assert_eq!(metadata.width, 499);
-        // assert_eq!(metadata.height, 375);
-        // assert_eq!(metadata.color_space, ImgColorSpace::RGB);
-        // assert_eq!(metadata.format, "JPEG");
+        let metadata = data.meta.image.unwrap();
+        assert_eq!(metadata.width, 499);
+        assert_eq!(metadata.height, 375);
+        assert_eq!(metadata.color_space, ImgColorSpace::RGB);
     }
 
     #[test]
     fn test_get_metadata_img_rgba() {
-        // let file = test::test_img_file_with_name("cat_rgba.png");
-        // let data = api::local::metadata::compute_metadata(file).unwrap();
+        let file = test::test_img_file_with_name("cat_rgba.png");
+        let data = api::local::metadata::compute_metadata(file).unwrap();
 
-        // assert_eq!(data.data_type, EntryDataType::Image);
-        // assert_eq!(data.mime_type, "image/png");
+        assert_eq!(data.data_type, EntryDataType::Image);
+        assert_eq!(data.mime_type, "image/png");
 
-        // let metadata = data.meta.image.unwrap();
+        let metadata = data.meta.image.unwrap();
 
-        // assert_eq!(metadata.width, 499);
-        // assert_eq!(metadata.height, 375);
-        // assert_eq!(metadata.color_space, ImgColorSpace::RGBA);
-        // assert_eq!(metadata.format, "PNG");
+        assert_eq!(metadata.width, 499);
+        assert_eq!(metadata.height, 375);
+        assert_eq!(metadata.color_space, ImgColorSpace::RGBA);
     }
 
     #[test]
     fn test_get_metadata_img_png_no_ext() {
-        // let file = test::test_img_file_with_name("cat_no_ext");
-        // let data = api::local::metadata::compute_metadata(file).unwrap();
+        let file = test::test_img_file_with_name("cat_no_ext");
+        let data = api::local::metadata::compute_metadata(file).unwrap();
 
-        // assert_eq!(data.data_type, EntryDataType::Image);
-        // assert_eq!(data.mime_type, "image/png");
-        // let metadata = data.meta.image.unwrap();
+        assert_eq!(data.data_type, EntryDataType::Image);
+        assert_eq!(data.mime_type, "image/png");
+        let metadata = data.meta.image.unwrap();
 
-        // assert_eq!(metadata.width, 499);
-        // assert_eq!(metadata.height, 375);
-        // assert_eq!(metadata.color_space, ImgColorSpace::RGBA);
-        // assert_eq!(metadata.format, "PNG");
+        assert_eq!(metadata.width, 499);
+        assert_eq!(metadata.height, 375);
+        assert_eq!(metadata.color_space, ImgColorSpace::RGBA);
     }
 
     #[test]
     fn test_get_metadata_img_grayscale() {
-        // let file = test::test_img_file_with_name("cat_grayscale.jpg");
+        let file = test::test_img_file_with_name("cat_grayscale.jpg");
 
-        // let data = api::local::metadata::compute_metadata(file).unwrap();
+        let data = api::local::metadata::compute_metadata(file).unwrap();
 
-        // assert_eq!(data.data_type, EntryDataType::Image);
-        // assert_eq!(data.mime_type, "image/jpeg");
+        assert_eq!(data.data_type, EntryDataType::Image);
+        assert_eq!(data.mime_type, "image/jpeg");
 
-        // let metadata = data.meta.image.unwrap();
+        let metadata = data.meta.image.unwrap();
 
-        // assert_eq!(metadata.width, 499);
-        // assert_eq!(metadata.height, 375);
-        // assert_eq!(metadata.color_space, ImgColorSpace::Grayscale);
-        // assert_eq!(metadata.format, "JPEG");
+        assert_eq!(metadata.width, 499);
+        assert_eq!(metadata.height, 375);
+        assert_eq!(metadata.color_space, ImgColorSpace::Grayscale);
     }
 
     #[test]
     fn test_get_metadata_img_mnist() {
-        // let file = test::test_img_file_with_name("mnist_7.png");
-        // let data = api::local::metadata::compute_metadata(file).unwrap();
+        let file = test::test_img_file_with_name("mnist_7.png");
+        let data = api::local::metadata::compute_metadata(file).unwrap();
 
-        // assert_eq!(data.data_type, EntryDataType::Image);
-        // assert_eq!(data.mime_type, "image/png");
+        assert_eq!(data.data_type, EntryDataType::Image);
+        assert_eq!(data.mime_type, "image/png");
 
-        // let metadata = data.meta.image.unwrap();
-        // assert_eq!(metadata.width, 28);
-        // assert_eq!(metadata.height, 28);
-        // assert_eq!(metadata.color_space, ImgColorSpace::Grayscale);
-        // assert_eq!(metadata.format, "PNG");
+        let metadata = data.meta.image.unwrap();
+        assert_eq!(metadata.width, 28);
+        assert_eq!(metadata.height, 28);
+        assert_eq!(metadata.color_space, ImgColorSpace::Grayscale);
     }
 }
--- oxen-rust/src/lib/src/core/db/df_db.rs
+++ oxen-rust/src/lib/src/core/db/df_db.rs
@@ -1,4 +1,92 @@
 //! Abstraction over DuckDB database to write and read dataframes from disk.
-//! 
+//!
 
+use crate::error::OxenError;
 
+use duckdb::arrow::record_batch::RecordBatch;
+use polars::prelude::*;
+use std::io::Cursor;
+use std::path::Path;
+
+/// Get a connection to a duckdb database.
+pub fn get_connection(path: impl AsRef<Path>) -> Result<duckdb::Connection, OxenError> {
+    let path = path.as_ref();
+    let conn = duckdb::Connection::open(path)?;
+    Ok(conn)
+}
+
+/// Query number of rows in a table.
+pub fn count(conn: &duckdb::Connection, table: &str) -> Result<usize, OxenError> {
+    let sql = format!("SELECT count(*) FROM {}", table);
+    let mut stmt = conn.prepare(&sql)?;
+    let mut rows = stmt.query([])?;
+    if let Some(row) = rows.next()? {
+        let size: usize = row.get(0)?;
+        Ok(size)
+    } else {
+        Err(OxenError::basic_str(format!("No rows in table {}", table)))
+    }
+}
+
+/// Select fields from a table.
+pub fn select(
+    conn: &duckdb::Connection,
+    table: &str,
+    fields: &[&str],
+    limit: usize,
+) -> Result<DataFrame, OxenError> {
+    let fields = fields.join(", ");
+    let sql = format!("SELECT {} FROM {} LIMIT {}", fields, table, limit);
+    log::debug!("select sql: {}", sql);
+    let mut stmt = conn.prepare(&sql)?;
+    let records: Vec<RecordBatch> = stmt.query_arrow([])?.collect();
+
+    // Hacky to convert to json and then to polars...but the results from these queries should be small, and
+    // if they are bigger, need to look into converting directly from arrow to polars.
+    let json = arrow_json::writer::record_batches_to_json_rows(&records[..]).unwrap();
+    let json_str = serde_json::to_string(&json).unwrap();
+
+    let content = Cursor::new(json_str.as_bytes());
+    let df = JsonReader::new(content).finish().unwrap();
+    log::debug!("result df: {:?}", df);
+
+    Ok(df)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_df_db_count() -> Result<(), OxenError> {
+        let db_file = Path::new("data")
+            .join("test")
+            .join("db")
+            .join("metadata.db");
+        let conn = get_connection(&db_file)?;
+
+        let count = count(&conn, "metadata")?;
+
+        assert_eq!(count, 16);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_df_db_select() -> Result<(), OxenError> {
+        let db_file = Path::new("data")
+            .join("test")
+            .join("db")
+            .join("metadata.db");
+        let conn = get_connection(&db_file)?;
+
+        let limit = 7;
+        let fields = vec!["filename", "data_type"];
+        let df = select(&conn, "metadata", &fields, limit)?;
+
+        assert!(df.width() == fields.len());
+        assert!(df.height() == limit);
+
+        Ok(())
+    }
+}
