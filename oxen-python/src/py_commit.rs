3992513921cc1a93f40a0821eb00b3231ea8d38f
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-07-14 17:31:50 -0700

    adding metadata to the pycommit object

--- oxen-python/src/py_commit.rs
+++ oxen-python/src/py_commit.rs
@@ -11,8 +11,41 @@ pub struct PyCommit {
 
 #[pymethods]
 impl PyCommit {
+    fn __repr__(&self) -> String {
+        format!("PyCommit(id={}, message={}, author={}, email={}, timestamp={}, parent_ids=[{}])", self.commit.id, self.commit.message, self.commit.author, self.commit.email, self.commit.timestamp, self.commit.parent_ids.join(", "))
+    }
+
+    fn __str__(&self) -> String {
+        self.commit.id.to_owned()
+    }
+
     #[getter]
-    pub fn commit_id(&self) -> PyResult<String> {
-        Ok(self.commit.id.to_string())
+    pub fn commit_id(&self) -> String {
+        self.commit.id.to_string()
+    }
+
+    #[getter]
+    pub fn message(&self) -> String {
+        self.commit.message.to_string()
+    }
+
+    #[getter]
+    pub fn author(&self) -> String {
+        self.commit.author.to_string()
+    }
+
+    #[getter]
+    pub fn email(&self) -> String {
+        self.commit.email.to_string()
+    }
+
+    #[getter]
+    pub fn timestamp(&self) -> String {
+        self.commit.timestamp.to_string()
+    }
+
+    #[getter]
+    pub fn parent_ids(&self) -> Vec<String> {
+        self.commit.parent_ids.to_owned()
     }
 }
