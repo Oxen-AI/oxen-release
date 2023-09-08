02450e5f5dc7e5478c703f06d17455eb07c54ce7
Ben Artuso <benr2so@gmail.com>
2023-09-08 08:58:28 -0400

    add post routes

--- oxen-rust/src/server/src/controllers/action.rs
+++ oxen-rust/src/server/src/controllers/action.rs
@@ -1,14 +1,32 @@
 use crate::{errors::OxenHttpError, params::path_param};
 use actix_web::{HttpRequest, HttpResponse};
+use liboxen::view::http::STATUS_SUCCESS;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct ActionResponse {
+    action: String,
+    status: String,
+    state: String,
+}
 
 pub async fn completed(req: HttpRequest) -> actix_web::Result<HttpResponse, OxenHttpError> {
     let action = path_param(&req, "action")?;
     log::debug!("{} action completed", action);
-    Ok(HttpResponse::Ok().finish())
+    let resp = ActionResponse {
+        action: action.to_string(),
+        state: "completed".to_string(),
+        status: STATUS_SUCCESS.to_string(),
+    };
+    Ok(HttpResponse::Ok().json(resp))
 }
 
 pub async fn started(req: HttpRequest) -> actix_web::Result<HttpResponse, OxenHttpError> {
     let action = path_param(&req, "action")?;
-    log::debug!("{} action started", action);
-    Ok(HttpResponse::Ok().finish())
+    let resp = ActionResponse {
+        action: action.to_string(),
+        status: STATUS_SUCCESS.to_string(),
+        state: "started".to_string(),
+    };
+    Ok(HttpResponse::Ok().json(resp))
 }
