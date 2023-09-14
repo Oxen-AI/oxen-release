8fca4e2d5d89e1d543fc2b1ae9658eef2c2f6113
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-09-14 16:21:12 -0700

    add the resolved commit to the schemas response

--- oxen-rust/src/lib/src/view/schema.rs
+++ oxen-rust/src/lib/src/view/schema.rs
@@ -1,19 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-use crate::model::Schema;
+use crate::model::{Commit, Schema};
 
 use super::StatusMessage;
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct SchemaResponse {
-    #[serde(flatten)]
-    pub status: StatusMessage,
-    pub schema: Schema,
-}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ListSchemaResponse {
     #[serde(flatten)]
     pub status: StatusMessage,
     pub schemas: Vec<Schema>,
+    pub commit: Option<Commit>,
 }
