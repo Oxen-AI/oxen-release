3150ad3d1894ccb8d9ce8467b2f09afb4ff440f9
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-10-09 09:03:49 -0700

    bump python 0.2.0 and liboxen 0.9.1

--- oxen-python/python/oxen/auth.py
+++ oxen-python/python/oxen/auth.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 def config_auth(host: str, token: str, path: Optional[str] = None):
     if path is None:
-        path = f"{util.get_oxen_home_dir()}/user_config.toml"
+        path = f"{util.get_oxen_config_dir()}/user_config.toml"
     if not path.endswith(".toml"):
         raise ValueError("Path must end with .toml")
     auth.add_host_auth(host, token, path)
