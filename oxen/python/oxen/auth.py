2b0dafc47263258c6ac57b89ef713c2a9da547c3
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-10-14 17:47:37 -0700

    add docstrings to auth and user

--- oxen/python/oxen/auth.py
+++ oxen/python/oxen/auth.py
@@ -2,6 +2,18 @@ from .oxen import auth, util
 from typing import Optional
 
 def config_auth(host: str, token: str, path: Optional[str] = None):
+    """
+    Configures authentication for a host.
+    
+    Args:
+        host: `str`
+            The host to configure authentication for. For example: 'hub.oxen.ai'
+        token: `str`
+            The token to use for authentication.
+        path: `Optional[str]`
+            The path to save the authentication config to.
+            Defaults to $HOME/.config/oxen/user_config.toml
+    """
     if path is None:
         path = f"{util.get_oxen_config_dir()}/user_config.toml"
     if not path.endswith(".toml"):
--- oxen/python/oxen/user.py
+++ oxen/python/oxen/user.py
@@ -2,6 +2,18 @@ from .oxen import user, util
 from typing import Optional
 
 def config_user(name: str, email: str, path: Optional[str] = None):
+    """
+    Configures user for a host.
+    
+    Args:
+        name: `str`
+            The name to use for user.
+        email: `str`
+            The email to use for user.
+        path: `Optional[str]`
+            The path to save the user config to.
+            Defaults to $HOME/.config/oxen/user_config.toml
+    """
     if path is None:
         path = f"{util.get_oxen_config_dir()}/user_config.toml"
     if not path.endswith(".toml"):
@@ -9,6 +21,14 @@ def config_user(name: str, email: str, path: Optional[str] = None):
     return user.config_user(name, email, path)
 
 def current_user(path: Optional[str] = None):
+    """
+    Gets the current user.
+    
+    Args:
+        path: `Optional[str]`
+            The path to load the user config from.
+            Defaults to $HOME/.config/oxen/user_config.toml
+    """
     if path is None:
         path = f"{util.get_oxen_config_dir()}/user_config.toml"
     if not path.endswith(".toml"):
