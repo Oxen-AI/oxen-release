897d33ea9519b067944973175627d12aa3151d24
Ben Artuso <benr2so@gmail.com>
2023-12-12 11:55:39 -0500

    server-side pre-push validation passing tests

--- oxen-rust/src/lib/src/core/cache/cachers/content_validator.rs
+++ oxen-rust/src/lib/src/core/cache/cachers/content_validator.rs
@@ -14,13 +14,16 @@ pub fn compute(repo: &LocalRepository, commit: &Commit) -> Result<(), OxenError>
     std::thread::sleep(std::time::Duration::from_millis(100));
 
     // Construct commit merkle tree if not sent by client
-    if !api::local::commits::has_merkle_tree(repo, commit)? {
-        log::debug!(
-            "Merkle tree for commit {} not provided by client, creating",
-            commit.id
-        );
-        api::local::commits::construct_commit_merkle_tree(repo, commit)?;
-    }
+    // if !api::local::commits::has_merkle_tree(repo, commit)? {
+    //     log::debug!(
+    //         "Merkle tree for commit {} not provided by client, creating",
+    //         commit.id
+    //     );
+    //     api::local::commits::construct_commit_merkle_tree(repo, commit)?;
+    // }
+
+    // TODONOW: find a better way to do this - maybe unpack all the obejcts from the commit
+    api::local::commits::new_construct_commit_merkle_tree(repo, commit)?;
 
     let tree_is_valid = commit_validator::validate_tree_hash(repo, commit)?;
 
