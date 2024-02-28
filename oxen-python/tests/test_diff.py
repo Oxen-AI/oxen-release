75b91107980fb5a41c8ac4693673437adc28a974
Greg Schoeninger <greg.schoeninger@gmail.com>
2024-02-27 21:54:48 -0800

    rename files for diff test

--- oxen-python/tests/test_diff.py
+++ oxen-python/tests/test_diff.py
@@ -3,12 +3,12 @@ import os
 from oxen import diff_tabular
 
 
-def test_add(shared_datadir):
+def test_diff_added_row(shared_datadir):
     repo_dir = os.path.join(shared_datadir, "Diffs")
 
     diff = diff_tabular(
-        left=os.path.join(repo_dir, "prompts_v1.csv"),
-        right=os.path.join(repo_dir, "prompts_v2.csv"),
+        left=os.path.join(repo_dir, "prompts.csv"),
+        right=os.path.join(repo_dir, "prompts_added_row.csv"),
         keys=[],  # No keys
         targets=[],  # No targets
     )
