0230dd2fb7108b6707840a94ea8aa82d283cdaeb
Greg Schoeninger <greg.schoeninger@gmail.com>
2024-02-29 20:48:48 -0800

    add df.load function and split up line_diff

--- oxen/src/df.rs
+++ oxen/src/df.rs
@@ -4,10 +4,19 @@
 use pyo3::prelude::*;
 use std::path::PathBuf;
 
-use liboxen::core::df::tabular;
+use liboxen::{core::df::tabular, opts::DFOpts};
 use crate::error::PyOxenError;
 
 use pyo3_polars::PyDataFrame;
+
+#[pyfunction]
+pub fn load(
+    path: PathBuf
+) -> Result<PyDataFrame, PyOxenError> {
+    let opts = DFOpts::empty();
+    let df = tabular::read_df(path, opts)?;
+    Ok(PyDataFrame(df))
+}
 
 #[pyfunction]
 pub fn save(
