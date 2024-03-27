9b3abfa1970425ef86c007c272c8426762830388
Greg Schoeninger <greg.schoeninger@gmail.com>
2024-03-27 12:37:14 -0600

    fix the hashing of paths to lookup vnodes

--- oxen-rust/src/lib/src/core/index/commit_dir_entry_reader.rs
+++ oxen-rust/src/lib/src/core/index/commit_dir_entry_reader.rs
@@ -128,8 +128,15 @@ impl CommitDirEntryReader {
     }
 
     pub fn has_file<P: AsRef<Path>>(&self, path: P) -> bool {
-        let full_path = self.dir.join(path.as_ref());
-        let path_hash_prefix = util::hasher::hash_path(full_path)[0..2].to_string();
+        let path = path.as_ref();
+        log::debug!("CommitDirEntryReader.has_file({:?})", path);
+
+        let full_path = self.dir.join(path);
+        // we have to make sure the full_path is `/` instead of `\\` to get the correct hashes
+        let full_path_str = full_path.to_str().unwrap().replace("\\", "/");
+
+        let path_hash_prefix = util::hasher::hash_path(&full_path_str)[0..2].to_string();
+        // log::debug!("CommitDirEntryReader.has_file({:?}) {:?} {}", path, full_path_str, path_hash_prefix);
 
         // Binary search for the appropriate vnode
         let vnode_child = self
@@ -140,33 +147,33 @@ impl CommitDirEntryReader {
         let Some(vnode_child) = vnode_child else {
             log::info!(
                 "could not get Some(vnode_child) for path {:?}",
-                path.as_ref()
+                path
             );
             return false;
         };
 
         // Get the vnode object proper
         let Ok(maybe_vnode) = self.object_reader.get_vnode(vnode_child.hash()) else {
-            log::info!("could not get Ok(maybe_vnode) for path {:?}", path.as_ref());
+            log::info!("could not get Ok(maybe_vnode) for path {:?}", path);
             return false;
         };
 
         let Some(vnode) = maybe_vnode else {
-            log::info!("could not get Some(vnode) for path {:?}", path.as_ref());
+            log::info!("could not get Some(vnode) for path {:?}", path);
             return false;
         };
 
         // Now binary search within the vnode for the appropriate file
-        let full_path = self.dir.join(path.as_ref());
+        let full_path = self.dir.join(path);
         log::debug!("Looking for full path: {:?}", full_path);
 
         let Ok(maybe_file) = vnode.binary_search_on_path(&full_path.to_path_buf()) else {
-            log::info!("could not get Ok(file) for path {:?}", path.as_ref());
+            log::info!("could not get Ok(file) for path {:?}", path);
             return false;
         };
 
         let Some(file) = maybe_file else {
-            log::info!("could not get Some(file) for path {:?}", path.as_ref());
+            log::info!("could not get Some(file) for path {:?}", path);
             return false;
         };
 
