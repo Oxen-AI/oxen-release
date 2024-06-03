43803dac87148ec25523ec1a9aa030952e3a30a6
Greg Schoeninger <greg.schoeninger@gmail.com>
2024-06-02 17:01:22 -0700

    add commit function to RemoteDataset and update polars deps

---
+++ scripts/index_twice.py
@@ -0,0 +1,14 @@
+
+from oxen import RemoteRepo
+from oxen import RemoteDataset
+from oxen.remote_dataset import index_dataset
+import time
+
+print("Creating Remote Repo")
+repo = RemoteRepo("ox/LLM-Dataset", "localhost:3001", scheme="http")
+
+print("Creating Remote Dataset")
+dataset = RemoteDataset(repo, "openhermes_train.parquet", index=True)
+
+print("Indexing dataset")
+index_dataset(repo, "openhermes_train.parquet")
