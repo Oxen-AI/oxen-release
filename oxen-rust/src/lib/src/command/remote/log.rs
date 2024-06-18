6e3e008fed1b5d9b0858e480ce9eb8bc09a04d4e
rpschoenburg <rpschoenburg@outlook.com>
2024-06-18 16:13:36 -0700

    Applied linters

--- oxen-rust/src/lib/src/command/remote/log.rs
+++ oxen-rust/src/lib/src/command/remote/log.rs
@@ -1,12 +1,9 @@
 use crate::api;
 use crate::error::OxenError;
-use crate::opts::LogOpts;
 use crate::model::commit::Commit;
 use crate::model::LocalRepository;
-
+use crate::opts::LogOpts;
 
 pub async fn log_commits(repo: &LocalRepository, opts: &LogOpts) -> Result<Vec<Commit>, OxenError> {
-
     api::local::commits::list_with_opts(repo, opts).await
-
 }
