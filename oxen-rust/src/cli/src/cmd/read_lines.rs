f1b2dbcee60c72922311f1699d36a0dac1cf79cd
rpschoenburg <rpschoenburg@outlook.com>
2024-06-18 20:30:46 -0700

    Applied linters

--- oxen-rust/src/cli/src/cmd/read_lines.rs
+++ oxen-rust/src/cli/src/cmd/read_lines.rs
@@ -1,7 +1,6 @@
 use async_trait::async_trait;
 use clap::{Arg, ArgMatches, Command};
 
-use liboxen::command;
 use liboxen::error::OxenError;
 use liboxen::util;
 use std::path::Path;
@@ -9,7 +8,6 @@ use std::path::Path;
 use crate::cmd::RunCmd;
 pub const NAME: &str = "read-lines";
 pub struct ReadLinesCmd;
-
 
 #[async_trait]
 impl RunCmd for ReadLinesCmd {
@@ -19,24 +17,24 @@ impl RunCmd for ReadLinesCmd {
 
     fn args(&self) -> Command {
         Command::new("read-lines")
-        .about("Read a set of lines from a file without loading it all into memory")
-        .arg(
-            Arg::new("PATH")
-                .help("Path to file you want to read")
-                .required(true)
-        )
-        .arg(
-            Arg::new("START")
-                .help("Start index of file")
-                .default_value("0")
-                .default_missing_value("0"),
-        )
-        .arg(
-            Arg::new("LENGTH")
-                .help("Length you want to read")
-                .default_value("10")
-                .default_missing_value("10"),
-        )
+            .about("Read a set of lines from a file without loading it all into memory")
+            .arg(
+                Arg::new("PATH")
+                    .help("Path to file you want to read")
+                    .required(true),
+            )
+            .arg(
+                Arg::new("START")
+                    .help("Start index of file")
+                    .default_value("0")
+                    .default_missing_value("0"),
+            )
+            .arg(
+                Arg::new("LENGTH")
+                    .help("Length you want to read")
+                    .default_value("10")
+                    .default_missing_value("10"),
+            )
     }
 
     async fn run(&self, args: &ArgMatches) -> Result<(), OxenError> {
@@ -51,7 +49,7 @@ impl RunCmd for ReadLinesCmd {
             .expect("Must supply LENGTH")
             .parse::<usize>()
             .expect("LENGTH must be a valid integer.");
-    
+
         let path = Path::new(path_str);
         let (lines, size) = util::fs::read_lines_paginated_ret_size(path, start, length);
         for line in lines.iter() {
