fbd610fc8f163e620dcbb6b9d85a6af60490fa36
rpschoenburg <rpschoenburg@outlook.com>
2024-06-20 16:44:19 -0700

    Applied Linters

--- oxen-rust/src/cli/src/cmd/remote/rm.rs
+++ oxen-rust/src/cli/src/cmd/remote/rm.rs
@@ -1,13 +1,13 @@
 use async_trait::async_trait;
 use clap::{Arg, ArgMatches, Command};
 
+use crate::helpers::check_repo_migration_needed;
+use liboxen::command;
 use liboxen::error::OxenError;
-use std::path::PathBuf;
-use std::env; 
 use liboxen::model::LocalRepository;
 use liboxen::opts::RmOpts;
-use liboxen::command;
-use crate::helpers::check_repo_migration_needed;
+use std::env;
+use std::path::PathBuf;
 
 use crate::cmd::RunCmd;
 pub const NAME: &str = "rm";
@@ -21,33 +21,33 @@ impl RunCmd for RemoteRmCmd {
 
     fn args(&self) -> Command {
         Command::new(NAME)
-        .about("Removes the specified files from the index")
-        .arg(
-            Arg::new("files")
-                .required(true)
-                .action(clap::ArgAction::Append),
-        )
-        .arg(
-            Arg::new("staged")
-                .long("staged")
-                .help("Removes the file from the staging area.")
-                .action(clap::ArgAction::SetTrue),
-        )
-        .arg(
-            Arg::new("recursive")
-                .long("recursive")
-                .short('r')
-                .help("Recursively removes directory.")
-                .action(clap::ArgAction::SetTrue),
-        )
+            .about("Removes the specified files from the index")
+            .arg(
+                Arg::new("files")
+                    .required(true)
+                    .action(clap::ArgAction::Append),
+            )
+            .arg(
+                Arg::new("staged")
+                    .long("staged")
+                    .help("Removes the file from the staging area.")
+                    .action(clap::ArgAction::SetTrue),
+            )
+            .arg(
+                Arg::new("recursive")
+                    .long("recursive")
+                    .short('r')
+                    .help("Recursively removes directory.")
+                    .action(clap::ArgAction::SetTrue),
+            )
     }
 
     async fn run(&self, args: &ArgMatches) -> Result<(), OxenError> {
         let paths: Vec<PathBuf> = args
-        .get_many::<String>("files")
-        .expect("Must supply files")
-        .map(PathBuf::from)
-        .collect();
+            .get_many::<String>("files")
+            .expect("Must supply files")
+            .map(PathBuf::from)
+            .collect();
 
         let opts = RmOpts {
             // The path will get overwritten for each file that is removed
