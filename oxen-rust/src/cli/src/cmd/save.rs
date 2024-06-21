f7e796a572fd4d8b2a73d5c84333c988931281a3
rpschoenburg <rpschoenburg@outlook.com>
2024-06-21 02:50:24 -0700

    Applied Linters

--- oxen-rust/src/cli/src/cmd/save.rs
+++ oxen-rust/src/cli/src/cmd/save.rs
@@ -2,11 +2,11 @@ use async_trait::async_trait;
 use clap::{Arg, ArgMatches, Command};
 
 use liboxen::command;
+use liboxen::error;
 use liboxen::error::OxenError;
 use liboxen::model::LocalRepository;
 use liboxen::util;
 use std::path::Path;
-use liboxen::error;
 
 use crate::cmd::RunCmd;
 pub const NAME: &str = "save";
@@ -20,33 +20,32 @@ impl RunCmd for SaveCmd {
 
     fn args(&self) -> Command {
         Command::new(NAME)
-        .arg(
-            Arg::new("PATH")
-                .help("Path of the local repository to save")
-                .required(true)
-                .index(1), // This represents the position of the argument in the command line command.
-        )
-        .arg(
-            Arg::new("output")
-                .help("Name of the output .tar.gz archive")
-                .short('o')
-                .long("output")
-                .required(true),
-        )
+            .arg(
+                Arg::new("PATH")
+                    .help("Path of the local repository to save")
+                    .required(true)
+                    .index(1), // This represents the position of the argument in the command line command.
+            )
+            .arg(
+                Arg::new("output")
+                    .help("Name of the output .tar.gz archive")
+                    .short('o')
+                    .long("output")
+                    .required(true),
+            )
     }
 
     async fn run(&self, args: &ArgMatches) -> Result<(), OxenError> {
         let repo_str = args.get_one::<String>("PATH").expect("Required");
         let output_str = args.get_one::<String>("output").expect("Required");
-    
+
         let output_path = Path::new(output_str);
         let repo_path = Path::new(repo_str);
         let repo_dir =
             util::fs::get_repo_root(repo_path).ok_or(OxenError::basic_str(error::NO_REPO_FOUND))?;
         let repo = LocalRepository::from_dir(&repo_dir)?;
-    
-        command::save(&repo, output_path)?;
 
+        command::save(&repo, output_path)?;
 
         Ok(())
     }
