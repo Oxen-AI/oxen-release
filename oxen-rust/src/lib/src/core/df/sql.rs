46042f73a8d328999380c10a8bbaacfaa27d5dee
eloymartinez <eloyalbmartinez@gmail.com>
2024-07-03 13:31:28 -0600

    fix clippy

--- oxen-rust/src/lib/src/core/df/sql.rs
+++ oxen-rust/src/lib/src/core/df/sql.rs
@@ -3,7 +3,7 @@ use polars::frame::DataFrame;
 use crate::{core::db::df_db, error::OxenError};
 
 pub fn query_df(sql: String, conn: &mut duckdb::Connection) -> Result<DataFrame, OxenError> {
-    let df = df_db::select_str(&conn, sql, false, None, None)?;
+    let df = df_db::select_str(conn, sql, false, None, None)?;
 
     Ok(df)
 }
