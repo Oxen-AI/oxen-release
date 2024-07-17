f496ad6325e3ec96070711a24344134c05af4f7e
rpschoenburg <rpschoenburg@outlook.com>
2024-07-17 03:28:01 -0700

    Applied Linters

--- oxen-rust/src/lib/src/core/df/pretty_print.rs
+++ oxen-rust/src/lib/src/core/df/pretty_print.rs
@@ -1,3 +1,4 @@
+use crate::core::df::tabular;
 /// # Pretty Print
 /// The default Polars DataFrame print implementation puts characters
 /// that do not fit well in our docs, so this transform helps cleanup the output.
@@ -20,7 +21,6 @@
 /// | 20*20  ┆ math     ┆ added             |
 /// +--------+----------+-------------------+
 use polars::prelude::*;
-use crate::core::df::tabular; 
 
 pub fn df_to_str(df: &DataFrame) -> String {
     let default_fmt = format!("{:?}", df);
@@ -36,11 +36,10 @@ pub fn df_to_str(df: &DataFrame) -> String {
 }
 
 pub fn df_to_str_full(df: &DataFrame) -> String {
-
-    let mut height = df.height(); 
-    let mut pos = 0; 
-    let mut ret_string = String::from(format!("shape: {:?}", df.shape())); 
-    ret_string.push_str("\n");
+    let mut height = df.height();
+    let mut pos = 0;
+    let mut ret_string = format!("shape: {:?}", df.shape());
+    ret_string.push('\n');
 
     let first_slice = tabular::slice_df(df.clone(), pos, pos + 10);
     let first_fmt = format!("{:?}", first_slice);
@@ -49,51 +48,50 @@ pub fn df_to_str_full(df: &DataFrame) -> String {
 
     for _ in 0..15 {
         ret_string.push_str(first_lines.next().unwrap());
-        ret_string.push_str("\n");  
-    }     
+        ret_string.push('\n');
+    }
 
     pos += 10;
-    height -= 10; 
+    height -= 10;
 
     while height > 10 {
-
         ret_string.push_str("+----------+---------------------------------+");
-        ret_string.push_str("\n");
+        ret_string.push('\n');
 
         let mid_slice = tabular::slice_df(df.clone(), pos, pos + 10);
         let mid_fmt = format!("{:?}", mid_slice);
         let mut lines = mid_fmt.lines();
 
         for _ in 0..6 {
-            lines.next(); 
+            lines.next();
         }
 
         for _ in 0..10 {
             ret_string.push_str(lines.next().unwrap());
-            ret_string.push_str("\n");  
+            ret_string.push('\n');
         }
 
         pos += 10;
-        height -= 10; 
+        height -= 10;
     }
 
     ret_string.push_str("+----------+---------------------------------+");
-    ret_string.push_str("\n");
+    ret_string.push('\n');
 
     let last_slice = tabular::slice_df(df.clone(), pos, pos + height);
     let last_fmt = format!("{:?}", last_slice);
     let mut last_lines = last_fmt.lines();
 
     for _ in 0..6 {
-        last_lines.next(); 
+        last_lines.next();
     }
 
     for _ in 0..height {
         ret_string.push_str(last_lines.next().unwrap());
-        ret_string.push_str("\n");  
+        ret_string.push('\n');
     }
     ret_string.push_str("+----------+---------------------------------+");
-    
+
     ret_string
         .replace(['┌', '└', '┬', '┴', '┐', '┘'], "+")
         .replace('─', "-")
