6f28f2ddfef40bed91f6fbbf1440f4f6bf46b92d
Greg Schoeninger <greg.schoeninger@gmail.com>
2024-07-25 17:33:50 -0700

    fix get commit history for file

--- oxen-rust/src/lib/src/opts/paginate_opts.rs
+++ oxen-rust/src/lib/src/opts/paginate_opts.rs
@@ -1,5 +1,17 @@
+use crate::constants::{DEFAULT_PAGE_NUM, DEFAULT_PAGE_SIZE};
+
 #[derive(Clone, Debug)]
 pub struct PaginateOpts {
     pub page_num: usize,
     pub page_size: usize,
+}
+
+// Add default values
+impl Default for PaginateOpts {
+    fn default() -> Self {
+        PaginateOpts {
+            page_num: DEFAULT_PAGE_NUM,
+            page_size: DEFAULT_PAGE_SIZE,
+        }
+    }
 }
