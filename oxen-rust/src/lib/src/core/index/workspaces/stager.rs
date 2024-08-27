131e05479e07c29cf3856d030379027d716ba893
Greg Schoeninger <greg.schoeninger@gmail.com>
2024-08-27 09:32:57 -0700

    remove use of Stager to compute status in workspace since we already mark the files as updated with rocksdb

--- oxen-rust/src/lib/src/core/index/workspaces/stager.rs
+++ oxen-rust/src/lib/src/core/index/workspaces/stager.rs
@@ -5,7 +5,6 @@ use std::path::{Path, PathBuf};
 use crate::constants::{FILES_DIR, MODS_DIR};
 use crate::core::db;
 use crate::core::db::key_val::str_json_db;
-use crate::core::index::{CommitEntryReader, Stager};
 use crate::error::OxenError;
 use crate::model::workspace::Workspace;
 use crate::model::StagedData;
@@ -14,40 +13,25 @@ fn files_db_path(workspace: &Workspace) -> PathBuf {
     workspace.dir().join(MODS_DIR).join(FILES_DIR)
 }
 
-pub fn status(workspace: &Workspace, directory: &Path) -> Result<StagedData, OxenError> {
-    let repo = &workspace.base_repo;
-    let workspace_repo = &workspace.workspace_repo;
-    let commit = &workspace.commit;
-    // Stager will be in the workspace repo
-    let stager = Stager::new(workspace_repo)?;
-    // But we will read from the commit in the main repo
-    log::debug!(
-        "list_staged_data get commit by id {} -> {} -> {:?}",
-        commit.message,
-        commit.id,
-        directory
-    );
+pub fn status(workspace: &Workspace, directory: impl AsRef<Path>) -> Result<StagedData, OxenError> {
+    log::debug!("workspaces::stager::status for workspace {:?} and directory {:?}", workspace.id, directory.as_ref());
 
-    log::debug!("index::workspaces::stager::status for dir {:?}", directory);
-    let reader = CommitEntryReader::new(repo, commit)?;
-    if Path::new(".") == directory {
-        let mut status = stager.status(&reader)?;
-        list_staged_entries(workspace, &mut status)?;
-        Ok(status)
-    } else {
-        let mut status = stager.status_from_dir(&reader, directory)?;
-        list_staged_entries(workspace, &mut status)?;
-        Ok(status)
-    }
+    let mut status = StagedData::empty();
+    list_staged_entries(workspace, directory.as_ref(), &mut status)?;
+    Ok(status)
 }
 
 // Modifications to files are staged in a separate DB and applied on commit,
 // so we fetch them from the mod_stager
-fn list_staged_entries(workspace: &Workspace, status: &mut StagedData) -> Result<(), OxenError> {
+fn list_staged_entries(workspace: &Workspace, directory: impl AsRef<Path>, status: &mut StagedData) -> Result<(), OxenError> {
     let mod_entries = list_files(workspace)?;
 
     for path in mod_entries {
-        status.modified_files.push(path.to_owned());
+        if Path::new(".") == directory.as_ref() {
+            status.modified_files.push(path.to_owned());
+        } else if path.starts_with(directory.as_ref()) {
+            status.modified_files.push(path.to_owned());
+        }
     }
 
     Ok(())
