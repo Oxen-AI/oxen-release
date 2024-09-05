d3e0d6f868626f702394ef88cc260843f19a5e2c
Joshua Elliott <joshuacelliott@gmail.com>
2024-09-05 10:49:30 -0600

    Fix some unit tests for checkout

--- oxen-rust/src/lib/src/repositories/checkout.rs
+++ oxen-rust/src/lib/src/repositories/checkout.rs
@@ -273,9 +273,9 @@ mod tests {
             repositories::add(&repo, &world_file)?;
             repositories::commit(&repo, "Added world.txt")?;
 
-            // Make sure we have both commits after the initial
+            // Make sure we have both commits
             let commits = repositories::commits::list(&repo)?;
-            assert_eq!(commits.len(), 3);
+            assert_eq!(commits.len(), 2);
 
             let branches = repositories::branches::list(&repo)?;
             assert_eq!(branches.len(), 2);
@@ -331,9 +331,9 @@ mod tests {
             repositories::add(&repo, &world_file)?;
             repositories::commit(&repo, "Added world.txt")?;
 
-            // Make sure we have both commits after the initial
+            // Make sure we have both commits
             let commits = repositories::commits::list(&repo)?;
-            assert_eq!(commits.len(), 3);
+            assert_eq!(commits.len(), 2);
 
             let branches = repositories::branches::list(&repo)?;
             assert_eq!(branches.len(), 2);
@@ -408,13 +408,13 @@ mod tests {
     #[tokio::test]
     async fn test_command_checkout_modified_file_in_subdirectory() -> Result<(), OxenError> {
         test::run_select_data_repo_test_no_commits_async("annotations", |repo| async move {
-            // Get the original branch name
-            let orig_branch = repositories::branches::current_branch(&repo)?.unwrap();
-
             // Track & commit the file
             let one_shot_path = repo.path.join("annotations/train/one_shot.csv");
             repositories::add(&repo, &one_shot_path)?;
             repositories::commit(&repo, "Adding one shot")?;
+
+            // Get the original branch name
+            let orig_branch = repositories::branches::current_branch(&repo)?.unwrap();
 
             // Get OG file contents
             let og_content = util::fs::read_from_path(&one_shot_path)?;
@@ -451,13 +451,13 @@ mod tests {
     async fn test_command_checkout_modified_file_from_fully_committed_repo() -> Result<(), OxenError>
     {
         test::run_select_data_repo_test_no_commits_async("annotations", |repo| async move {
-            // Get the original branch name
-            let orig_branch = repositories::branches::current_branch(&repo)?.unwrap();
-
             // Track & commit all the data
             let one_shot_path = repo.path.join("annotations/train/one_shot.csv");
             repositories::add(&repo, &repo.path)?;
             repositories::commit(&repo, "Adding one shot")?;
+
+            // Get the original branch name
+            let orig_branch = repositories::branches::current_branch(&repo)?.unwrap();
 
             // Get OG file contents
             let og_content = util::fs::read_from_path(&one_shot_path)?;
@@ -496,9 +496,6 @@ mod tests {
     #[tokio::test]
     async fn test_command_remove_dir_then_revert() -> Result<(), OxenError> {
         test::run_select_data_repo_test_no_commits_async("train", |repo| async move {
-            // Get the original branch name
-            let orig_branch = repositories::branches::current_branch(&repo)?.unwrap();
-
             // (dir already created in helper)
             let dir_to_remove = repo.path.join("train");
             let og_num_files = util::fs::rcount_files_in_dir(&dir_to_remove);
@@ -507,7 +504,10 @@ mod tests {
             repositories::add(&repo, &dir_to_remove)?;
             repositories::commit(&repo, "Adding train dir")?;
 
-            // Create a branch to make the changes
+            // Get the original branch name
+            let orig_branch = repositories::branches::current_branch(&repo)?.unwrap();
+
+            // Create a new branch to make the changes
             let branch_name = "feature/removing-train";
             repositories::branches::create_checkout(&repo, branch_name)?;
 
