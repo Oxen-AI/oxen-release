3917baa2ee9cb95c8fcde9285985f7eed2deb03b
rpschoenburg <rpschoenburg@outlook.com>
2024-09-20 00:30:18 -0700

    Removed outdated download logic

--- oxen-rust/src/lib/src/download.rs
+++ oxen-rust/src/lib/src/download.rs
@@ -43,7 +43,7 @@ pub async fn download_dir(
         }
         MinOxenVersion::V0_19_0 => {
             log::debug!("Calling v0_19_0 download_dir with remote_repo: {:?}, entry: {:?}, local_path: {:?} ", remote_repo, entry, local_path);
-            core::v0_19_0::download::download_dir(remote_repo, entry, local_path);
+            core::v0_19_0::download::download_dir(repo, remote_repo, entry, local_path);
         }   
     }
 
