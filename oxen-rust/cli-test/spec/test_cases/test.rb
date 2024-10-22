32d497737056d88b8f51f1715fa52615d2f0b0dc
eloymartinez <eloyalbmartinez@gmail.com>
2024-10-22 09:40:11 -0400

    Add conditions on time execution for performance test

--- oxen-rust/cli-test/spec/test_cases/test.rb
+++ oxen-rust/cli-test/spec/test_cases/test.rb
@@ -3,7 +3,7 @@ require 'dotenv'
 
 RSpec.describe 'test', type: :aruba do
   before(:each) do
-    aruba.config.exit_timeout = 6000 # Set the timeout to 600 seconds
+    aruba.config.exit_timeout = 6000 # Set the timeout to 6000 seconds
   end
 
   after(:each) do
@@ -16,30 +16,44 @@ RSpec.describe 'test', type: :aruba do
     cd 'test-small-repo'
 
     # Generate image repository
-    system('python ../benchmark/generate_image_repo.py --output_dir ~/test-small-repo/Data/10k_images --num_images 100000 --num_dirs 10 --image_size 128 128')
+    system('python ../benchmark/generate_image_repo.py --output_dir ~/test-small-repo/Data/10k_images --num_images 1 --num_dirs 1 --image_size 128 128')
 
     # Initialize the repository
-    puts "oxen init command took: #{measure_time('oxen init')} seconds"
+    init_time = measure_time('oxen init')
+    puts "oxen init command took: #{init_time} seconds"
+    expect(init_time).to be < 1.0 # Example threshold
 
     # Add the file
-    puts "oxen add command took: #{measure_time('oxen add .')} seconds"
+    add_time = measure_time('oxen add .')
+    puts "oxen add command took: #{add_time} seconds"
+    expect(add_time).to be < 35.0 # Example threshold
 
     # Commit the file
-    puts "oxen commit command took: #{measure_time('oxen commit -m "Add small file"')} seconds"
+    commit_time = measure_time('oxen commit -m "Add small file"')
+    puts "oxen commit command took: #{commit_time} seconds"
+    expect(commit_time).to be < 70.0 # Example threshold
 
     # Create the remote repository
-    puts "oxen create-remote command took: #{measure_time('oxen create-remote --name EloyMartinez/performance-test --host dev.hub.oxen.ai --scheme https --is_public')} seconds"
+    create_remote_time = measure_time('oxen create-remote --name EloyMartinez/performance-test --host dev.hub.oxen.ai --scheme https --is_public')
+    puts "oxen create-remote command took: #{create_remote_time} seconds"
+    expect(create_remote_time).to be < 1.0 # Example threshold
 
     # Set the remote
-    puts "oxen config --set-remote command took: #{measure_time('oxen config --set-remote origin https://dev.hub.oxen.ai/EloyMartinez/performance-test')} seconds"
+    set_remote_time = measure_time('oxen config --set-remote origin https://dev.hub.oxen.ai/EloyMartinez/performance-test')
+    puts "oxen config --set-remote command took: #{set_remote_time} seconds"
+    expect(set_remote_time).to be < 0.5 # Example threshold
 
     # Push the file
-    puts "oxen push command took: #{measure_time('oxen push')} seconds"
+    push_time = measure_time('oxen push')
+    puts "oxen push command took: #{push_time} seconds"
+    expect(push_time).to be < 800.0 # Example threshold
 
     cd '..'
 
     # Clone the repository
-    puts "oxen clone command took: #{measure_time('oxen clone https://dev.hub.oxen.ai/EloyMartinez/performance-test')} seconds"
+    clone_time = measure_time('oxen clone https://dev.hub.oxen.ai/EloyMartinez/performance-test')
+    puts "oxen clone command took: #{clone_time} seconds"
+    expect(clone_time).to be < 650.0 # Example threshold
 
     directory_path = 'tmp/aruba/performance-test'
     file_path = File.join(directory_path, 'simple.txt')
@@ -55,17 +69,29 @@ RSpec.describe 'test', type: :aruba do
     cd 'performance-test'
 
     # Add, commit, and push the changes
-    puts "oxen add simple.txt command took: #{measure_time('oxen add simple.txt')} seconds"
-    puts "oxen commit command took: #{measure_time('oxen commit -m "Edit simple text file remotely"')} seconds"
-    puts "oxen push command took: #{measure_time('oxen push')} seconds"
+    add_simple_time = measure_time('oxen add simple.txt')
+    puts "oxen add simple.txt command took: #{add_simple_time} seconds"
+    expect(add_simple_time).to be < 3.0 # Example threshold
+
+    commit_simple_time = measure_time('oxen commit -m "Edit simple text file remotely"')
+    puts "oxen commit command took: #{commit_simple_time} seconds"
+    expect(commit_simple_time).to be < 3.0 # Example threshold
+
+    push_simple_time = measure_time('oxen push')
+    puts "oxen push command took: #{push_simple_time} seconds"
+    expect(push_simple_time).to be < 300.0 # Example threshold
 
     # Pull the changes from test-repo
     cd '..'
     cd 'test-small-repo'
-    puts "oxen pull command took: #{measure_time('oxen pull')} seconds"
+    pull_time = measure_time('oxen pull')
+    puts "oxen pull command took: #{pull_time} seconds"
+    expect(pull_time).to be < 100.0 # Example threshold
 
     # Checkout to a new branch
-    puts "oxen checkout command took: #{measure_time('oxen checkout -b second_branch')} seconds"
+    checkout_time = measure_time('oxen checkout -b second_branch')
+    puts "oxen checkout command took: #{checkout_time} seconds"
+    expect(checkout_time).to be < 1.0 # Example threshold
 
     directory_path = 'tmp/aruba/test-small-repo'
     file_path = File.join(directory_path, 'simple.txt')
@@ -79,19 +105,36 @@ RSpec.describe 'test', type: :aruba do
     end
 
     # Add, commit, and push the second change
-    puts "oxen add simple.txt command took: #{measure_time('oxen add simple.txt')} seconds"
-    puts "oxen commit command took: #{measure_time('oxen commit -m "Edit simple text file remotely second time"')} seconds"
-    puts "oxen push command took: #{measure_time('oxen push')} seconds"
+    add_second_change_time = measure_time('oxen add simple.txt')
+    puts "oxen add simple.txt command took: #{add_second_change_time} seconds"
+    expect(add_second_change_time).to be < 1.0 # Example threshold
+
+    commit_second_change_time = measure_time('oxen commit -m "Edit simple text file remotely second time"')
+    puts "oxen commit command took: #{commit_second_change_time} seconds"
+    expect(commit_second_change_time).to be < 1.0 # Example threshold
+
+    push_second_change_time = measure_time('oxen push')
+    puts "oxen push command took: #{push_second_change_time} seconds"
+    expect(push_second_change_time).to be < 20.0 # Example threshold
 
     # Checkout main branch and then back to second branch
-    puts "oxen checkout main command took: #{measure_time('oxen checkout main')} seconds"
-    puts "oxen checkout second_branch command took: #{measure_time('oxen checkout second_branch')} seconds"
+    checkout_main_branch_time = measure_time('oxen checkout main')
+    puts "oxen checkout main command took: #{checkout_main_branch_time} seconds"
+    expect(checkout_main_branch_time).to be < 100.0 # Example threshold
+
+    checkout_second_branch_time = measure_time('oxen checkout second_branch')
+    puts "oxen checkout second_branch command took: #{checkout_second_branch_time} seconds"
+    expect(checkout_second_branch_time).to be < 100.0 # Example threshold
 
     # Restore the file from the main branch
-    puts "oxen restore command took: #{measure_time('oxen restore --source main simple.txt')} seconds"
+    restore_second_change_time = measure_time('oxen restore --source main simple.txt')
+    puts "oxen restore command took: #{restore_second_change_time} seconds"
+    expect(restore_second_change_time).to be < 3.0 # Example threshold
 
     # Delete the remote repository
-    puts "oxen delete-remote command took: #{measure_time('oxen delete-remote --name EloyMartinez/performance-test --host dev.hub.oxen.ai')} seconds"
+    delete_remote_time = measure_time('oxen delete-remote --name EloyMartinez/performance-test --host dev.hub.oxen.ai')
+    puts "oxen delete-remote command took: #{delete_remote_time} seconds"
+    expect(delete_remote_time).to be < 2.0 # Example threshold
   end
 
   def measure_time(command)
