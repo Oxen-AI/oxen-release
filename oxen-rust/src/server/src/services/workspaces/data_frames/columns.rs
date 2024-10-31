efde9c17b98bcfb8699173b21bee9217dd3db708
eloymartinez <eloyalbmartinez@gmail.com>
2024-10-31 16:46:01 -0400

    create metadata api endpoint

--- oxen-rust/src/server/src/services/workspaces/data_frames/columns.rs
+++ oxen-rust/src/server/src/services/workspaces/data_frames/columns.rs
@@ -10,6 +10,10 @@ pub fn columns() -> Scope {
             web::post().to(controllers::workspaces::data_frames::columns::create),
         )
         .route(
+            "/schema/metadata/{path:.*}",
+            web::post().to(controllers::workspaces::data_frames::columns::add_column_metadata),
+        )
+        .route(
             "{column_name:.*}/resource/{path:.*}",
             web::delete().to(controllers::workspaces::data_frames::columns::delete),
         )
