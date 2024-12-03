fa4d237a09228617db12da0b7305d4d0cb13d2f2
eloymartinez <eloyalbmartinez@gmail.com>
2024-12-03 10:34:05 -0500

    optimise dir download on download command

--- oxen-rust/src/lib/src/view/tree.rs
+++ oxen-rust/src/lib/src/view/tree.rs
@@ -1,4 +1,6 @@
+pub mod merkle_hash;
 pub mod merkle_hashes;
 pub mod nodes;
 
+pub use crate::view::tree::merkle_hash::MerkleHashResponse;
 pub use crate::view::tree::merkle_hashes::MerkleHashesResponse;
