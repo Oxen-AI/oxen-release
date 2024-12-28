b81a8892c7b3dfe78e72eceb8d01e60d7109731d
eloymartinez <eloyalbmartinez@gmail.com>
2024-12-28 13:35:05 -0500

    remove redondant child loop inside r_download_entries

--- oxen-rust/src/lib/src/core/v0_19_0/download.rs
+++ oxen-rust/src/lib/src/core/v0_19_0/download.rs
@@ -61,11 +61,11 @@ async fn r_download_entries(
 ) -> Result<(), OxenError> {
     log::debug!("downloading entries for {:?}", directory);
 
-    if let EMerkleTreeNode::VNode(_) = &node.node {
-        let mut entries: Vec<Entry> = vec![];
+    let mut entries: Vec<Entry> = vec![];
 
-        for child in &node.children {
-            if let EMerkleTreeNode::File(file_node) = &child.node {
+    for child in &node.children {
+        match &child.node {
+            EMerkleTreeNode::File(file_node) => {
                 entries.push(Entry::CommitEntry(CommitEntry {
                     commit_id: file_node.last_commit_id.to_string(),
                     path: directory.join(&file_node.name),
@@ -75,8 +75,26 @@ async fn r_download_entries(
                     last_modified_nanoseconds: file_node.last_modified_nanoseconds,
                 }));
             }
-        }
+            EMerkleTreeNode::Directory(dir_node) => {
+                let mut new_directory = directory.to_path_buf();
+                new_directory.push(&dir_node.name);
 
+                if child.has_children() {
+                    Box::pin(r_download_entries(
+                        remote_repo,
+                        local_repo_path,
+                        child,
+                        &new_directory,
+                        pull_progress,
+                    ))
+                    .await?;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    if !entries.is_empty() {
         log::debug!("downloading {} entries to working dir", entries.len());
         core::v0_10_0::index::puller::pull_entries_to_working_dir(
             remote_repo,
@@ -85,25 +103,6 @@ async fn r_download_entries(
             pull_progress,
         )
         .await?;
-    }
-    for child in &node.children {
-        log::debug!("downloading entry {:?}", child.hash);
-
-        let mut new_directory = directory.to_path_buf();
-        if let EMerkleTreeNode::Directory(dir_node) = &child.node {
-            new_directory.push(&dir_node.name);
-        }
-
-        if child.has_children() {
-            Box::pin(r_download_entries(
-                remote_repo,
-                local_repo_path,
-                child,
-                &new_directory,
-                pull_progress,
-            ))
-            .await?;
-        }
     }
 
     Ok(())
