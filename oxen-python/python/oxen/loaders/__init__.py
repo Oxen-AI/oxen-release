8d5ec7472346274d0faa721987864a7f44697939
Greg Schoeninger <greg.schoeninger@gmail.com>
2025-01-16 10:55:12 -0800

    strip out dag, ops, loaders and unnessasary dependencies. keep oxen light and nimble

--- oxen-python/python/oxen/loaders/__init__.py
+++
@@ -1,7 +0,0 @@
-# Loaders
-from .chat import ChatLoader
-from .regression import RegressionLoader
-from .image_classification import ImageClassificationLoader
-
-# Names of public modules we want to expose
-__all__ = ["ChatLoader", "RegressionLoader", "ImageClassificationLoader"]
