1c5f9a5728b94761e8a3a3fe680877ee78426630
Joshua Elliott <j@oxen.ai>
2025-01-30 12:00:35 -0700

    Add OxenFS docs and fix parquet writing

--- oxen-python/tests/test_fsspec_backend.py
+++ oxen-python/tests/test_fsspec_backend.py
@@ -1,5 +1,7 @@
 import os
 import fsspec
+import pandas as pd
+
 from oxen import RemoteRepo
 
 
@@ -18,11 +20,8 @@ def test_fsspec_read_file(
     with fs.open("images/1.jpg", mode="rb") as f:
         remote_image_file = f.read()
 
-    local_image_file = open(
-        os.path.join(shared_datadir, "CelebA/images/1.jpg"), "rb"
-    ).read()
-
-    assert remote_image_file == local_image_file
+    with open(os.path.join(shared_datadir, "CelebA/images/1.jpg"), "rb") as f:
+        assert remote_image_file == f.read()
 
 
 def test_fsspec_write_file(
@@ -45,5 +44,33 @@ def test_fsspec_write_file(
     assert len(remote_repo.log()) == 2
 
     local_repo.pull()
-    updated_prompt = open(os.path.join(local_repo.path, "prompt.txt"), "r").read()
-    assert updated_prompt == "This is an updated prompt!"
+    with open(os.path.join(local_repo.path, "prompt.txt"), "r") as f:
+        assert f.read() == "This is an updated prompt!"
+
+
+def test_fsspec_write_file_with_pandas(
+    chat_bot_remote_repo_fully_pushed: RemoteRepo, shared_datadir
+):
+    local_repo, remote_repo = chat_bot_remote_repo_fully_pushed
+    # examples.tsv pushed in setup
+    namespace = remote_repo._repo.namespace()
+    repo_name = remote_repo._repo.name()
+    host = remote_repo._repo.host
+    scheme = remote_repo._repo.scheme
+    fs = fsspec.filesystem(
+        "oxen", namespace=namespace, repo=repo_name, host=host, scheme=scheme
+    )
+    # Read TSV
+    with fs.open("examples.tsv") as f:
+        df = pd.read_csv(f, delimiter="\t")
+
+    assert df.shape == (6, 2)
+
+    # Write as parquet
+    with fs.open("examples.parquet", mode="wb") as f:
+        df.to_parquet(f)
+
+    local_repo.pull()
+    with open(os.path.join(local_repo.path, "examples.parquet"), "rb") as f:
+        df_new = pd.read_parquet(f)
+        assert df_new.equals(df)
