7ace1d887000ae4fb10a6dfeb8bb875e73d1f588
Greg Schoeninger <greg.schoeninger@gmail.com>
2025-02-03 08:49:19 -0800

    fix branch not found message

--- oxen-rust/src/server/src/controllers/workspaces.rs
+++ oxen-rust/src/server/src/controllers/workspaces.rs
@@ -33,7 +33,7 @@ pub async fn get_or_create(
     };
 
     let Some(branch) = repositories::branches::get_by_name(&repo, &data.branch_name)? else {
-        return Ok(HttpResponse::BadRequest().json(StatusMessage::error("Branch not found")));
+        return Ok(HttpResponse::BadRequest().json(StatusMessage::error(format!("Branch not found: {}", data.branch_name))));
     };
 
     // Return workspace if it already exists
@@ -109,7 +109,7 @@ pub async fn create(
     };
 
     let Some(branch) = repositories::branches::get_by_name(&repo, &data.branch_name)? else {
-        return Ok(HttpResponse::BadRequest().json(StatusMessage::error("Branch not found")));
+        return Ok(HttpResponse::BadRequest().json(StatusMessage::error(format!("Branch not found: {}", data.branch_name))));
     };
 
     let workspace_id = &data.workspace_id;
