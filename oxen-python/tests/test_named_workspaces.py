39b86d61ec04ce6d75dca7913f4f898a34480a9a
eloymartinez <eloyalbmartinez@gmail.com>
2025-02-10 14:35:03 -0500

    add tests

--- oxen-python/tests/test_named_workspaces.py
+++ oxen-python/tests/test_named_workspaces.py
@@ -14,7 +14,7 @@ def test_commit_to_named_workspace(
     workspace.add(image_path_1)
     assert len(remote_repo.list_workspaces()) == 1
     # Commit the changes
-    workspace.commit("Adding a new image to the feature branch", should_delete=True)
+    workspace.commit("Adding a new image to the feature branch")
     assert len(remote_repo.list_workspaces()) == 1
     workspace = Workspace(remote_repo, branch="main", workspace_name="my-workspace")
     assert workspace.name() == "my-workspace"
@@ -32,4 +32,27 @@ def test_named_workspace_naming_collision(
     workspace = Workspace(remote_repo, branch="main", workspace_id=workspace.id())
     workspace = Workspace(remote_repo, branch="main", workspace_name=workspace.id())
     assert workspace.name() == "my-workspace"
+    assert len(remote_repo.list_workspaces()) == 1
+
+
+def test_named_workspace_naming_collision(
+    celeba_remote_repo_one_image_pushed: RemoteRepo, shared_datadir
+):
+    _, remote_repo = celeba_remote_repo_one_image_pushed
+    workspace = Workspace(remote_repo, branch="main", workspace_name="my-workspace")
+
+    # Create the file inside the shared data directory.
+    stock_file_path = os.path.join(shared_datadir, "DownloadTest", "stock.exs")
+    os.makedirs(os.path.dirname(stock_file_path), exist_ok=True)
+    with open(stock_file_path, "w") as file:
+        file.write("")
+
+    # Simulate changes and commits multiple times.
+    for i in range(3):
+        with open(stock_file_path, "a") as file:
+            file.write("something\n")
+        workspace.add(stock_file_path)
+        workspace.commit("message")
+
+    # Ensure that only one workspace exists.
     assert len(remote_repo.list_workspaces()) == 1
--- oxen-python/tests/test_workspace_list.py
+++ oxen-python/tests/test_workspace_list.py
@@ -18,14 +18,14 @@ def test_commit_to_new_workspace(
     assert len(remote_repo.list_workspaces()) == 1
 
     # Commit the changes
-    workspace.commit("Adding a new image to the feature branch", should_delete=True)
+    workspace.commit("Adding a new image to the feature branch")
     assert len(remote_repo.list_workspaces()) == 0
 
     # Create a second workspace and add another file
     workspace2 = Workspace(remote_repo, "main")
     image_path_2 = os.path.join(shared_datadir, "CelebA/images/3.jpg")
     workspace2.add(image_path_2)
-    workspace2.commit("Adding a new image to the feature branch", should_delete=False)
+    workspace2.commit("Adding a new image to the feature branch")
     workspaces = remote_repo.list_workspaces()
     print(workspaces)
-    assert len(workspaces) == 1
+    assert len(workspaces) == 0
