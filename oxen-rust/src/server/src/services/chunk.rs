589e3a572273f45cf3b1cb89c838af3b4d5c726e
Xiaoran <lilian.zhou26@outlook.com>
2025-02-13 12:26:37 -0800

    Delete head func for download chunk

--- oxen-rust/src/server/src/services/chunk.rs
+++ oxen-rust/src/server/src/services/chunk.rs
@@ -4,13 +4,8 @@ use actix_web::Scope;
 use crate::controllers;
 
 pub fn chunk() -> Scope {
-    web::scope("/chunk")
-        .route(
-            "/{resource:.*}",
-            web::get().to(controllers::entries::download_chunk),
-        )
-        .route(
-            "/{resource:.*}",
-            web::head().to(controllers::entries::download_chunk),
-        )
+    web::scope("/chunk").route(
+        "/{resource:.*}",
+        web::get().to(controllers::entries::download_chunk),
+    )
 }
