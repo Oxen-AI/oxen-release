bbd112a47fcfb37e97df811b2d450f7aa104e8e1
rpschoenburg <rpschoenburg@outlook.com>
2025-03-12 11:54:46 -0700

    Use system independent paths for test comparisons
    Uses pathlib's PurePath to represent every multi-component path
    across the unit tests, allowing them to pass on both Linux/MacOS
    and Windows

--- oxen/tests/test_workspace_add_to_branch.py
+++ oxen/tests/test_workspace_add_to_branch.py
@@ -1,5 +1,6 @@
 import os
 from oxen import Workspace
+from pathlib import PurePath
 
 
 def test_workspace_add_to_branch(celeba_remote_repo_one_image_pushed, shared_datadir):
@@ -9,7 +10,9 @@ def test_workspace_add_to_branch(celeba_remote_repo_one_image_pushed, shared_dat
 
     workspace = Workspace(remote_repo, "newbranch")
 
-    full_path = os.path.join(shared_datadir, "CelebA/images/1.jpg")
+    image = str(PurePath("CelebA", "images", "1.jpg"))
+
+    full_path = os.path.join(shared_datadir, image)
     workspace.add(full_path)
     status = workspace.status()
 
