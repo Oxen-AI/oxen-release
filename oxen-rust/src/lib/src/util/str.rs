ae9ab6d521106bd6a0f001ba01422abaaf973145
Greg Schoeninger <greg.schoeninger@gmail.com>
2025-04-10 11:00:16 +0200

    add to_ordinal and better messaging on downloading chunk error

--- oxen-rust/src/lib/src/util/str.rs
+++ oxen-rust/src/lib/src/util/str.rs
@@ -5,3 +5,15 @@ pub fn split_and_trim(input: &str, delimiter: &str) -> Vec<String> {
         .map(String::from)
         .collect::<Vec<String>>()
 }
+
+/// Converts a number to its ordinal string representation (1st, 2nd, 3rd, etc.)
+pub fn to_ordinal(n: u64) -> String {
+    let suffix = match (n % 10, n % 100) {
+        (1, 11) | (2, 12) | (3, 13) => "th",
+        (1, _) => "st",
+        (2, _) => "nd",
+        (3, _) => "rd",
+        _ => "th",
+    };
+    format!("{}{}", n, suffix)
+}
