50565dcef8c4711729a1dd45894379e840f178ec
Joshua Elliott <j@oxen.ai>
2025-04-15 10:52:35 -0600

    Remove RefReader and RefWriter

--- oxen-rust/src/lib/src/core/refs.rs
+++ oxen-rust/src/lib/src/core/refs.rs
@@ -1,9 +1,3 @@
-// pub mod ref_db_reader;
 pub mod ref_manager;
-// pub mod ref_reader;
-// pub mod ref_writer;
 
-// pub use ref_db_reader::RefDBReader;
 pub use ref_manager::with_ref_manager;
-// pub use ref_reader::RefReader;
-// pub use ref_writer::with_ref_writer;
