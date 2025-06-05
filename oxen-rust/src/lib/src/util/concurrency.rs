fc0c01c0ddb5bcdf43d52b584014cd075d8f24c8
Joshua Elliott <j@oxen.ai>
2025-06-05 16:31:44 -0600

    Improve num_threads_for_items
    It will try to use the OXEN_NUM_THREADS override, but always use
    min(num_threads, num_items)

--- oxen-rust/src/lib/src/util/concurrency.rs
+++ oxen-rust/src/lib/src/util/concurrency.rs
@@ -4,21 +4,17 @@ use crate::constants;
 /// Can be overridden by setting the environment variable OXEN_NUM_THREADS
 /// Defaults to constants::DEFAULT_NUM_WORKERS or the number of CPUs we have if we have less than that
 pub fn num_threads_for_items(num_items: usize) -> usize {
-    // If the environment variable is set, use that
-    if let Ok(num_threads) = std::env::var("OXEN_NUM_THREADS") {
+    let num_workers = if let Ok(num_threads) = std::env::var("OXEN_NUM_THREADS") {
+        // If the environment variable is set, use that
         if let Ok(num_threads) = num_threads.parse::<usize>() {
-            return num_threads;
+            num_threads
+        } else {
+            // If parsing failed, fall back to default logic
+            get_default_num_workers()
         }
-    }
-
-    // Then check how many CPUs we have
-    let num_cpus = num_cpus::get();
-
-    // Default to constants::DEFAULT_NUM_WORKERS, but if we have less cpus than that, use that instead
-    let num_workers = if constants::DEFAULT_NUM_WORKERS > num_cpus {
-        num_cpus
     } else {
-        constants::DEFAULT_NUM_WORKERS
+        // Environment variable not set, use default logic
+        get_default_num_workers()
     };
 
     // Finally look at how many items we have, and if we have less items than workers, use that instead
@@ -26,5 +22,17 @@ pub fn num_threads_for_items(num_items: usize) -> usize {
         num_items
     } else {
         num_workers
+    }
+}
+
+fn get_default_num_workers() -> usize {
+    // Check how many CPUs we have
+    let num_cpus = num_cpus::get();
+
+    // Default to constants::DEFAULT_NUM_WORKERS, but if we have less cpus than that, use that instead
+    if constants::DEFAULT_NUM_WORKERS > num_cpus {
+        num_cpus
+    } else {
+        constants::DEFAULT_NUM_WORKERS
     }
 }
