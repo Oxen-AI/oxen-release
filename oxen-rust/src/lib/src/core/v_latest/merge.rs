bef9cb3534fa328bfa2b9ce65720d35800289249
Surya Ganesh <suriyaganesh097@gmail.com>
2025-06-20 13:48:05 -0400

    add tests and fix merge skipping merge with same head commit

--- oxen-rust/src/lib/src/core/v_latest/merge.rs
+++ oxen-rust/src/lib/src/core/v_latest/merge.rs
@@ -447,10 +447,6 @@ fn fast_forward_merge(
         ));
     };
 
-    if merge_tree.hash == base_tree.hash {
-        return Err(OxenError::basic_str("both branches at same commit"));
-    }
-
     // Stop early if there are conflicts
     let mut merge_tree_results = MergeResult::new();
     let mut seen_files = HashSet::new();
